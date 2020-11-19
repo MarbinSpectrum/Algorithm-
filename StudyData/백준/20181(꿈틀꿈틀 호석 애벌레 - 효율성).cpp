76f4f6d00fc4394ada9de41a5e5b7d94b049fa9c
MarbinSpectrum <9507ym@naver.com>
2020-11-19 17:37:07 +0900

    20181

---
+++ StudyData/백준/20181(꿈틀꿈틀 호석 애벌레 - 효율성).cpp
@@ -0,0 +1,138 @@
+﻿#ifndef _GLIBCXX_NO_ASSERT 
+#include <cassert> 
+#endif #include <cctype>
+#include <cerrno> 
+#include <cfloat> 
+#include <ciso646> 
+#include <climits> 
+#include <clocale> 
+#include <cmath> 
+#include <csetjmp> 
+#include <csignal>
+#include <cstdarg> 
+#include <cstddef>
+#include <cstdio> 
+#include <cstdlib> 
+#include <cstring> 
+#include <ctime> 
+
+#if __cplusplus >= 201103L 
+#include <ccomplex> 
+#include <cfenv> 
+#include <cinttypes> 
+#include <cstdalign> 
+#include <cstdbool>
+#include <cstdint> 
+#include <ctgmath>
+#include <cwchar> 
+#include <cwctype>
+#endif 
+
+// C++ 
+#include <algorithm> 
+#include <bitset> 
+#include <complex> 
+#include <deque> 
+#include <exception> 
+#include <fstream> 
+#include <functional> 
+#include <iomanip> 
+#include <ios> 
+#include <iosfwd> 
+#include <iostream> 
+#include <istream> 
+#include <iterator> 
+#include <limits> 
+#include <list> 
+#include <locale> 
+#include <map> 
+#include <memory> 
+#include <new> 
+#include <numeric>
+#include <ostream> 
+#include <queue> 
+#include <set> 
+#include <sstream> 
+#include <stack> 
+#include <stdexcept> 
+#include <streambuf> 
+#include <string> 
+#include <typeinfo> 
+#include <utility> 
+#include <valarray> 
+#include <vector> 
+
+#if __cplusplus >= 201103L 
+#include <array> 
+#include <atomic> 
+#include <chrono> 
+#include <condition_variable> 
+#include <forward_list> 
+#include <future> 
+#include <initializer_list> 
+#include <mutex> 
+#include <random> 
+#include <ratio>
+#include <regex> 
+#include <scoped_allocator> 
+#include <system_error> 
+#include <thread> 
+#include <tuple> 
+#include <typeindex> 
+#include <type_traits> 
+#include <unordered_map> 
+#include <unordered_set> 
+#endif
+
+#pragma warning(disable:4996)
+
+using namespace std;
+
+#define endl "\n"
+#define int long long
+#define Debug(a,b) cout << a << " " << b << endl
+#define Init(a,b) memset(a,b,sizeof(a))
+
+const int INF = 0x3fffffff;
+const int Dic[4][2] = { {+1,+0},{-1,+0},{0,+1},{0,-1} };
+
+////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+int N,K;
+int Arr[100001];
+int DP[100001];
+
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N >> K;
+    for (int i = 0; i < N; i++)
+        cin >> Arr[i];
+
+    int s = 0;
+    int stack = 0;
+    int m = 0;
+    for (int i = 0; i < N; i++)
+    {
+        m = max(m, DP[i]);
+        while (s < N && stack < K)
+        {
+            stack += Arr[s];
+            s++;
+        }
+        DP[s] = max(DP[s], m + max((int)0, stack - K));
+        stack -= Arr[i];
+    }
+    int R = 0;
+    for (int i = 0; i < 100001; i++)
+        R = max(R, DP[i]);
+    cout << R << endl;
+  
+
+}
