0708e6e8435b5628e71c55974b6dcbbfbfa23afc
MarbinSpectrum <9507ym@naver.com>
2020-11-20 14:34:47 +0900

    2157

---
+++ StudyData/백준/2157(여행).cpp
@@ -0,0 +1,145 @@
+﻿#ifndef _GLIBCXX_NO_ASSERT 
+#include <cassert> 
+#endif #include <cctype>
+#include <cerrno> 
+#include <cfloat> 
+#include <ciso646> 
+#include <climits> 
+#include <clocale> 
+#include <cmath> 
+#include <csetjmp> 
+#include <csignal>
+#include <cstdarg> 
+#include <cstddef>
+#include <cstdio> 
+#include <cstdlib> 
+#include <cstring> 
+#include <ctime> 
+
+#if __cplusplus >= 201103L 
+#include <ccomplex> 
+#include <cfenv> 
+#include <cinttypes> 
+#include <cstdalign> 
+#include <cstdbool>
+#include <cstdint> 
+#include <ctgmath>
+#include <cwchar> 
+#include <cwctype>
+#endif 
+
+// C++ 
+#include <algorithm> 
+#include <bitset> 
+#include <complex> 
+#include <deque> 
+#include <exception> 
+#include <fstream> 
+#include <functional> 
+#include <iomanip> 
+#include <ios> 
+#include <iosfwd> 
+#include <iostream> 
+#include <istream> 
+#include <iterator> 
+#include <limits> 
+#include <list> 
+#include <locale> 
+#include <map> 
+#include <memory> 
+#include <new> 
+#include <numeric>
+#include <ostream> 
+#include <queue> 
+#include <set> 
+#include <sstream> 
+#include <stack> 
+#include <stdexcept> 
+#include <streambuf> 
+#include <string> 
+#include <typeinfo> 
+#include <utility> 
+#include <valarray> 
+#include <vector> 
+
+#if __cplusplus >= 201103L 
+#include <array> 
+#include <atomic> 
+#include <chrono> 
+#include <condition_variable> 
+#include <forward_list> 
+#include <future> 
+#include <initializer_list> 
+#include <mutex> 
+#include <random> 
+#include <ratio>
+#include <regex> 
+#include <scoped_allocator> 
+#include <system_error> 
+#include <thread> 
+#include <tuple> 
+#include <typeindex> 
+#include <type_traits> 
+#include <unordered_map> 
+#include <unordered_set> 
+#endif
+
+#pragma warning(disable:4996)
+
+using namespace std;
+
+#define endl "\n"
+#define int long long
+#define Debug(a,b) cout << a << " " << b << endl
+#define Init(a,b) memset(a,b,sizeof(a))
+
+const int INF = 0x3fffffff;
+const int Dic[4][2] = { {+1,+0},{-1,+0},{0,+1},{0,-1} };
+
+////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+int N, M, K;
+vector<pair<int,int>> Dist[301];
+int DP[301][301];
+bool Visit[301][301];
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N >> M >> K;
+    Visit[1][1] = true;
+    for (int k = 0; k < K; k++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a < b)
+            Dist[a].push_back({ b,c });
+    }
+
+    for (int m = 1; m <= M; m++)
+    {
+        for (int n = 1; n <= N; n++)
+        {
+            if (Visit[n][m])
+            {
+                for (int i = 0; i < Dist[n].size(); i++)
+                {
+                    int next = Dist[n][i].first;
+                    int point = Dist[n][i].second;
+                    Visit[next][m + 1] = true;
+                    DP[next][m + 1] = max(DP[next][m + 1], DP[n][m] + point);
+                }
+            }
+        }
+    }
+
+    int Result = 0;
+    for (int m = 1; m <= M; m++)
+        Result = max(Result, DP[N][m]);
+
+    cout << Result << endl;
+}
