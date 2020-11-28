910386fdeb45e0d58fa4afd4549dbcea157ada99
MarbinSpectrum <9507ym@naver.com>
2020-11-28 20:08:21 +0900

    1693

---
+++ StudyData/백준/1693(트리 색칠하기).cpp
@@ -0,0 +1,149 @@
+﻿#ifndef _GLIBCXX_NO_ASSERT 
+#include <cassert> 
+#endif #include <cctype>
+#include <cerrno> 
+#include <cfloat> 
+#include <ciso646> 
+#include <climits> 
+#include <clocale> 
+#include <cmath> 
+#include <csetjmp> 
+#include <csignal>
+#include <cstdarg> 
+#include <cstddef>
+#include <cstdio> 
+#include <cstdlib> 
+#include <cstring> 
+#include <ctime> 
+
+#if __cplusplus >= 201103L 
+#include <ccomplex> 
+#include <cfenv> 
+#include <cinttypes> 
+#include <cstdalign> 
+#include <cstdbool>
+#include <cstdint> 
+#include <ctgmath>
+#include <cwchar> 
+#include <cwctype>
+#endif 
+
+// C++ 
+#include <algorithm> 
+#include <bitset> 
+#include <complex> 
+#include <deque> 
+#include <exception> 
+#include <fstream> 
+#include <functional> 
+#include <iomanip> 
+#include <ios> 
+#include <iosfwd> 
+#include <iostream> 
+#include <istream> 
+#include <iterator> 
+#include <limits> 
+#include <list> 
+#include <locale> 
+#include <map> 
+#include <memory> 
+#include <new> 
+#include <numeric>
+#include <ostream> 
+#include <queue> 
+#include <set> 
+#include <sstream> 
+#include <stack> 
+#include <stdexcept> 
+#include <streambuf> 
+#include <string> 
+#include <typeinfo> 
+#include <utility> 
+#include <valarray> 
+#include <vector> 
+
+#if __cplusplus >= 201103L 
+#include <array> 
+#include <atomic> 
+#include <chrono> 
+#include <condition_variable> 
+#include <forward_list> 
+#include <future> 
+#include <initializer_list> 
+#include <mutex> 
+#include <random> 
+#include <ratio>
+#include <regex> 
+#include <scoped_allocator> 
+#include <system_error> 
+#include <thread> 
+#include <tuple> 
+#include <typeindex> 
+#include <type_traits> 
+#include <unordered_map> 
+#include <unordered_set> 
+#endif
+
+#pragma warning(disable:4996)
+
+using namespace std;
+
+#define endl "\n"
+#define int long long
+#define Debug(a,b) cout << a << " " << b << endl
+#define Init(a,b) memset(a,b,sizeof(a))
+
+const int INF = 0x3fffffff;
+const int Dic[4][2] = { {+1,+0},{-1,+0},{0,+1},{0,-1} };
+
+////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+int N;
+const int COLOR = 20;
+vector<int> Arr[1000001];
+int DP[100001][COLOR + 1];
+int D(int par, int now, int color)
+{
+    if (DP[now][color] != -1)
+        return DP[now][color];
+    DP[now][color] = color;
+    for (int i = 0; i < Arr[now].size(); i++)
+    {
+        int next = Arr[now][i];
+        if (next == par)
+            continue;
+        int temp = INF;
+        for (int j = 1; j <= COLOR; j++)
+        {
+            if (color == j)
+                continue;
+            temp = min(temp, D(now, next, j));
+        }
+        DP[now][color] += temp;
+    }
+    return  DP[now][color];
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    for (int i = 1; i <= N - 1; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        Arr[a].push_back(b);
+        Arr[b].push_back(a);
+    }
+    Init(DP, -1);
+
+    int R = INF;
+    for (int j = 1; j <= COLOR; j++)
+        R = min(R, D(0, 1, j));
+    cout << R << endl;
+}
