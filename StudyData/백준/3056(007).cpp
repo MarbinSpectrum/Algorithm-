b3686d154f96b3eeb48804f5fdd3fb006a67debd
MarbinSpectrum <9507ym@naver.com>
2020-12-02 02:47:04 +0900

    3056

---
+++ StudyData/백준/3056(007).cpp
@@ -0,0 +1,163 @@
+﻿#ifndef _GLIBCXX_NO_ASSERT 
+#include <cassert> 
+#endif #include <cctype>
+#include <cerrno> 
+#include <cfloat> 
+#include <ciso646> 
+#include <climits> 
+#include <clocale> 
+#include <cmath> 
+#include <csetjmp> 
+#include <csignal>
+#include <cstdarg> 
+#include <cstddef>
+#include <cstdio> 
+#include <cstdlib> 
+#include <cstring> 
+#include <ctime> 
+
+#if __cplusplus >= 201103L 
+#include <ccomplex> 
+#include <cfenv> 
+#include <cinttypes> 
+#include <cstdalign> 
+#include <cstdbool>
+#include <cstdint> 
+#include <ctgmath>
+#include <cwchar> 
+#include <cwctype>
+#endif 
+
+// C++ 
+#include <algorithm> 
+#include <bitset> 
+#include <complex> 
+#include <deque> 
+#include <exception> 
+#include <fstream> 
+#include <functional> 
+#include <iomanip> 
+#include <ios> 
+#include <iosfwd> 
+#include <iostream> 
+#include <istream> 
+#include <iterator> 
+#include <limits> 
+#include <list> 
+#include <locale> 
+#include <map> 
+#include <memory> 
+#include <new> 
+#include <numeric>
+#include <ostream> 
+#include <queue> 
+#include <set> 
+#include <sstream> 
+#include <stack> 
+#include <stdexcept> 
+#include <streambuf> 
+#include <string> 
+#include <typeinfo> 
+#include <utility> 
+#include <valarray> 
+#include <vector> 
+
+#if __cplusplus >= 201103L 
+#include <array> 
+#include <atomic> 
+#include <chrono> 
+#include <condition_variable> 
+#include <forward_list> 
+#include <future> 
+#include <initializer_list> 
+#include <mutex> 
+#include <random> 
+#include <ratio>
+#include <regex> 
+#include <scoped_allocator> 
+#include <system_error> 
+#include <thread> 
+#include <tuple> 
+#include <typeindex> 
+#include <type_traits> 
+#include <unordered_map> 
+#include <unordered_set> 
+#endif
+
+#pragma warning(disable:4996)
+
+using namespace std;
+
+#define endl "\n"
+#define int long long
+#define Debug(a,b) cout << a << " " << b << endl
+#define Init(a,b) memset(a,b,sizeof(a))
+
+int GCD(int a, int b)
+{
+    if (b == 0)
+        return a;
+    else
+        return GCD(b, a % b);
+}
+
+const int INF = 0x3fffffff;
+const int Dic[4][2] = { {+1,+0},{-1,+0},{0,+1},{0,-1} };
+
+////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+int N;
+double DP[2000001];
+double A[21][21];
+int Digit(int bit)
+{
+    int v = 0;
+    for (int i = 1; i <= N; i++)
+        if (bit & (1 << (i - 1)))
+            v++;
+    return v;
+}
+double D(int bit)
+{
+    int n = Digit(bit) + 1;
+    if (n > N)
+        return 1;
+    if (DP[bit] != -1)
+        return DP[bit];
+
+    for (int i = 1; i <= N; i++)
+    {
+        if (bit & (1 << (i - 1)))
+            continue;
+        DP[bit] = max(DP[bit], A[n][i] * D(bit | (1 << i - 1)));
+    }
+    return DP[bit];
+}
+
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    for (int j = 0; j <= 2000000; j++)
+        DP[j] = -1;
+
+    cin >> N;
+    for (int i = 1; i <= N; i++)
+        for (int j = 1; j <= N; j++)
+        {
+            cin >> A[i][j];
+            A[i][j] /= 100;
+        }
+
+    cout.precision(6);
+    cout << fixed;
+    cout << D(0) * 100;
+
+}
+
+
