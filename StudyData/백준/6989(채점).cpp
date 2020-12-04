3d8ee794f17a3a5804141f5a9ecef815d9827ace
MarbinSpectrum <9507ym@naver.com>
2020-12-04 15:53:05 +0900

    6989

---
+++ StudyData/백준/6989(채점).cpp
@@ -0,0 +1,166 @@
+﻿#ifndef _GLIBCXX_NO_ASSERT 
+#include <cassert> 
+#endif #include <cctype>
+#include <cerrno> 
+#include <cfloat> 
+#include <ciso646> 
+#include <climits> 
+#include <clocale> 
+#include <cmath> 
+#include <csetjmp> 
+#include <csignal>
+#include <cstdarg> 
+#include <cstddef>
+#include <cstdio> 
+#include <cstdlib> 
+#include <cstring> 
+#include <ctime> 
+
+#if __cplusplus >= 201103L 
+#include <ccomplex> 
+#include <cfenv> 
+#include <cinttypes> 
+#include <cstdalign> 
+#include <cstdbool>
+#include <cstdint> 
+#include <ctgmath>
+#include <cwchar> 
+#include <cwctype>
+#endif 
+
+// C++ 
+#include <algorithm> 
+#include <bitset> 
+#include <complex> 
+#include <deque> 
+#include <exception> 
+#include <fstream> 
+#include <functional> 
+#include <iomanip> 
+#include <ios> 
+#include <iosfwd> 
+#include <iostream> 
+#include <istream> 
+#include <iterator> 
+#include <limits> 
+#include <list> 
+#include <locale> 
+#include <map> 
+#include <memory> 
+#include <new> 
+#include <numeric>
+#include <ostream> 
+#include <queue> 
+#include <set> 
+#include <sstream> 
+#include <stack> 
+#include <stdexcept> 
+#include <streambuf> 
+#include <string> 
+#include <typeinfo> 
+#include <utility> 
+#include <valarray> 
+#include <vector> 
+
+#if __cplusplus >= 201103L 
+#include <array> 
+#include <atomic> 
+#include <chrono> 
+#include <condition_variable> 
+#include <forward_list> 
+#include <future> 
+#include <initializer_list> 
+#include <mutex> 
+#include <random> 
+#include <ratio>
+#include <regex> 
+#include <scoped_allocator> 
+#include <system_error> 
+#include <thread> 
+#include <tuple> 
+#include <typeindex> 
+#include <type_traits> 
+#include <unordered_map> 
+#include <unordered_set> 
+#endif
+
+#pragma warning(disable:4996)
+
+using namespace std;
+
+#define endl "\n"
+#define int long long
+#define Debug(a,b) cout << a << " " << b << endl
+#define Init(a,b) memset(a,b,sizeof(a))
+
+int GCD(int a, int b)
+{
+    if (b == 0)
+        return a;
+    else
+        return GCD(b, a % b);
+}
+
+const int INF = 0x3fffffff;
+const int Dic[4][2] = { {+1,+0},{-1,+0},{0,+1},{0,-1} };
+
+////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+int N, K;
+int P[152];
+int S[152];
+int Sum[152][152];
+
+bitset<1140000> DP[152];
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    for (int i = 1; i <= N; i++)
+        cin >> P[i];
+    cin >> K;
+
+    for (int i = 1; i <= N; i++)
+        S[i] = S[i - 1] + P[i];
+    for (int i = 1; i <= N; i++)
+        for (int j = i; j <= N; j++)
+            Sum[i][j] = Sum[i][j - 1] + S[j] - S[i - 1];
+
+    if (K > Sum[1][N])
+    {
+        cout << K << endl;
+        return 0;
+    }
+    else
+    {
+        DP[0].set(0);
+        DP[1].set(0);
+        DP[1].set(P[1]);
+
+        for (int i = 2; i <= N; i++)
+        {
+            for (int j = 1; j <= i + 1; j++)
+            {
+                if (j <= 2)
+                    DP[i].set(Sum[j][i]);
+                else
+                    DP[i] |= (DP[j - 2] << Sum[j][i]);
+            }
+
+        }
+
+        int Result = K;
+        while (DP[N].test(Result))
+            Result++;
+
+        cout << Result << endl;
+    }
+}
+
+
