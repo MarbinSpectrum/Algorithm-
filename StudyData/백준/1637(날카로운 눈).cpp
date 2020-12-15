b411af2bcd8d995cef039eb75667aea4f64d7b49
MarbinSpectrum <9507ym@naver.com>
2020-12-15 19:02:19 +0900

    1637

---
+++ StudyData/백준/1637(날카로운 눈).cpp
@@ -0,0 +1,163 @@
+﻿#ifndef _GLIBCXX_NO_ASSERT 
+#include <cassert> 
+#endif #include <cctype>
+#include <cerrno> 
+#include <cfloat> 
+#include <ciso646> 
+#include <climits> 
+#include <clocale> 
+#include <cmath> 
+#include <csetjmp> 
+#include <csignal>
+#include <cstdarg> 
+#include <cstddef>
+#include <cstdio> 
+#include <cstdlib> 
+#include <cstring> 
+#include <ctime> 
+
+#if __cplusplus >= 201103L 
+#include <ccomplex> 
+#include <cfenv> 
+#include <cinttypes> 
+#include <cstdalign> 
+#include <cstdbool>
+#include <cstdint> 
+#include <ctgmath>
+#include <cwchar> 
+#include <cwctype>
+#endif 
+
+// C++ 
+#include <algorithm> 
+#include <bitset> 
+#include <complex> 
+#include <deque> 
+#include <exception> 
+#include <fstream> 
+#include <functional> 
+#include <iomanip> 
+#include <ios> 
+#include <iosfwd> 
+#include <iostream> 
+#include <istream> 
+#include <iterator> 
+#include <limits> 
+#include <list> 
+#include <locale> 
+#include <map> 
+#include <memory> 
+#include <new> 
+#include <numeric>
+#include <ostream> 
+#include <queue> 
+#include <set> 
+#include <sstream> 
+#include <stack> 
+#include <stdexcept> 
+#include <streambuf> 
+#include <string> 
+#include <typeinfo> 
+#include <utility> 
+#include <valarray> 
+#include <vector> 
+
+#if __cplusplus >= 201103L 
+#include <array> 
+#include <atomic> 
+#include <chrono> 
+#include <condition_variable> 
+#include <forward_list> 
+#include <future> 
+#include <initializer_list> 
+#include <mutex> 
+#include <random> 
+#include <ratio>
+#include <regex> 
+#include <scoped_allocator> 
+#include <system_error> 
+#include <thread> 
+#include <tuple> 
+#include <typeindex> 
+#include <type_traits> 
+#include <unordered_map> 
+#include <unordered_set> 
+#endif
+
+#pragma warning(disable:4996)
+
+using namespace std;
+
+#define endl "\n"
+#define int long long
+#define Debug(a,b) cout << a << " " << b << endl
+#define Init(a,b) memset(a,b,sizeof(a))
+
+int GCD(int a, int b)
+{
+    if (b == 0)
+        return a;
+    else
+        return GCD(b, a % b);
+}
+
+const int INF = 0x3fffffff;
+const int Dic[4][2] = { {+1,+0},{-1,+0},{0,+1},{0,-1} };
+
+////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+int N;
+vector<tuple<int, int, int>> Arr;
+
+int GetValue(tuple<int, int, int> t, int e)
+{
+    int a = get<0>(t);
+    int b = get<1>(t);
+    int c = get<2>(t);
+    return max((long long)0, (min(e, c) - a) / b + 1);
+}
+
+int GetArrValue(int e)
+{
+    int sum = 0;
+    for (int i = 0; i < N; i++)
+        sum += GetValue(Arr[i], e);
+    return sum;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        int a, b, c;
+        cin >> a >> c >> b;
+        Arr.push_back(make_tuple(a, b, c));       
+    }
+
+    int left = 0;
+    int right = 2147483647;
+    int mid = 0;
+    while (left < right)
+    {
+        mid = (left + right) / 2;
+        if (GetArrValue(mid) & 1)
+            right = mid;
+        else
+            left = mid + 1;
+    }
+
+    int num = GetArrValue(right) - GetArrValue(right - 1);
+    if (num & 1)
+        Debug(right, num);
+    else
+        cout << "NOTHING" << endl;
+}
+
+
