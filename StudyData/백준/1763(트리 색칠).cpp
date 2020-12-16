23b72055e6db59d001264c5aa30d2a10f7b98331
MarbinSpectrum <9507ym@naver.com>
2020-12-17 00:07:52 +0900

    1763

---
+++ StudyData/백준/1763(트리 색칠).cpp
@@ -0,0 +1,180 @@
+﻿#ifndef _GLIBCXX_NO_ASSERT 
+#include <cassert> 
+#endif #include <cctype>
+#include <cerrno> 
+#include <cfloat> 
+#include <ciso646> 
+#include <climits> 
+#include <clocale> 
+#include <cmath> 
+#include <csetjmp> 
+#include <csignal>
+#include <cstdarg> 
+#include <cstddef>
+#include <cstdio> 
+#include <cstdlib> 
+#include <cstring> 
+#include <ctime> 
+
+#if __cplusplus >= 201103L 
+#include <ccomplex> 
+#include <cfenv> 
+#include <cinttypes> 
+#include <cstdalign> 
+#include <cstdbool>
+#include <cstdint> 
+#include <ctgmath>
+#include <cwchar> 
+#include <cwctype>
+#endif 
+
+// C++ 
+#include <algorithm> 
+#include <bitset> 
+#include <complex> 
+#include <deque> 
+#include <exception> 
+#include <fstream> 
+#include <functional> 
+#include <iomanip> 
+#include <ios> 
+#include <iosfwd> 
+#include <iostream> 
+#include <istream> 
+#include <iterator> 
+#include <limits> 
+#include <list> 
+#include <locale> 
+#include <map> 
+#include <memory> 
+#include <new> 
+#include <numeric>
+#include <ostream> 
+#include <queue> 
+#include <set> 
+#include <sstream> 
+#include <stack> 
+#include <stdexcept> 
+#include <streambuf> 
+#include <string> 
+#include <typeinfo> 
+#include <utility> 
+#include <valarray> 
+#include <vector> 
+
+#if __cplusplus >= 201103L 
+#include <array> 
+#include <atomic> 
+#include <chrono> 
+#include <condition_variable> 
+#include <forward_list> 
+#include <future> 
+#include <initializer_list> 
+#include <mutex> 
+#include <random> 
+#include <ratio>
+#include <regex> 
+#include <scoped_allocator> 
+#include <system_error> 
+#include <thread> 
+#include <tuple> 
+#include <typeindex> 
+#include <type_traits> 
+#include <unordered_map> 
+#include <unordered_set> 
+#endif
+
+#pragma warning(disable:4996)
+
+using namespace std;
+
+#define endl "\n"
+#define int long long
+#define Debug(a,b) cout << a << " " << b << endl
+#define Init(a,b) memset(a,b,sizeof(a))
+
+int GCD(int a, int b)
+{
+    if (b == 0)
+        return a;
+    else
+        return GCD(b, a % b);
+}
+
+const int INF = 0x3fffffff;
+const int Dic[4][2] = { {+1,+0},{-1,+0},{0,+1},{0,-1} };
+
+////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+int N, R;
+int C[1001];
+vector<int> Child[1001];
+int Parent[1001];
+bool Visit[1001];
+int CN[1001];
+
+int Find()
+{
+    int index = -1;
+    float MinValue = -INF;
+    for (int i = 1; i <= N; i++)
+    {
+        float temp = (float)C[i] / CN[i];
+        if (!Visit[i] && i != R && MinValue < temp)
+        {
+            MinValue = temp;
+            index = i;
+        }
+    }
+
+    return index;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N >> R;
+    for (int i = 1; i <= N; i++)
+        cin >> C[i];
+    for (int i = 0; i < N - 1; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        Child[a].push_back(b);
+        Parent[b] = a;
+    }
+    Parent[R] = -1;
+    for (int i = 1; i <= N; i++)
+        CN[i] = 1;
+
+    int Result = 0;
+    for (int i = 0; i < N - 1; i++)
+    {
+        int a = Find();
+        Visit[a] = true;
+        int b = Parent[a];
+        while(Visit[b])
+            b = Parent[b];
+        Result += C[a] * CN[b];
+
+        CN[b] += CN[a];
+        C[b] += C[a];
+        Parent[a] = b;
+        for (int i = 0; i < Child[a].size(); i++)
+        {
+            int next = Child[a][i];
+            Parent[next] = b;
+        }
+    }
+    Result += C[R];
+
+    cout << Result << endl;
+
+}
+
+
