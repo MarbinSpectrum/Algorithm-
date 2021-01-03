f1e1610494d50c361adfcdbab269b1794fb009a2
MarbinSpectrum <9507ym@naver.com>
2021-01-04 05:22:56 +0900

    14428

--- StudyData/백준/14428(수열과 쿼리 16).cpp
+++ StudyData/백준/14428(수열과 쿼리 16).cpp
@@ -1,14 +1,110 @@
-﻿#include<iostream>
-#include<math.h>
-#include<algorithm>
-#include<vector>
-#include<queue>
-#include<stack>
-#include<string>
-#include <string.h>
-#include <set>
+﻿#ifndef _GLIBCXX_NO_ASSERT 
+#include <cassert> 
+#endif #include <cctype>
+#include <cerrno> 
+#include <cfloat> 
+#include <ciso646> 
+#include <climits> 
+#include <clocale> 
+#include <cmath> 
+#include <csetjmp> 
+#include <csignal>
+#include <cstdarg> 
+#include <cstddef>
+#include <cstdio> 
+#include <cstdlib> 
+#include <cstring> 
+#include <ctime> 
+
+#if __cplusplus >= 201103L 
+#include <ccomplex> 
+#include <cfenv> 
+#include <cinttypes> 
+#include <cstdalign> 
+#include <cstdbool>
+#include <cstdint> 
+#include <ctgmath>
+#include <cwchar> 
+#include <cwctype>
+#endif 
+
+// C++ 
+#include <algorithm> 
+#include <bitset> 
+#include <complex> 
+#include <deque> 
+#include <exception> 
+#include <fstream> 
+#include <functional> 
+#include <iomanip> 
+#include <ios> 
+#include <iosfwd> 
+#include <iostream> 
+#include <istream> 
+#include <iterator> 
+#include <limits> 
+#include <list> 
+#include <locale> 
+#include <map> 
+#include <memory> 
+#include <new> 
+#include <numeric>
+#include <ostream> 
+#include <queue> 
+#include <set> 
+#include <sstream> 
+#include <stack> 
+#include <stdexcept> 
+#include <streambuf> 
+#include <string> 
+#include <typeinfo> 
+#include <utility> 
+#include <valarray> 
+#include <vector> 
+
+#if __cplusplus >= 201103L 
+#include <array> 
+#include <atomic> 
+#include <chrono> 
+#include <condition_variable> 
+#include <forward_list> 
+#include <future> 
+#include <initializer_list> 
+#include <mutex> 
+#include <random> 
+#include <ratio>
+#include <regex> 
+#include <scoped_allocator> 
+#include <system_error> 
+#include <thread> 
+#include <tuple> 
+#include <typeindex> 
+#include <type_traits> 
+#include <unordered_map> 
+#include <unordered_set> 
+#endif
 
 #pragma warning(disable:4996)
+
+using namespace std;
+
+#define endl "\n"
+#define int long long
+#define Debug(a,b) cout << a << " " << b << endl
+#define Init(a,b) memset(a,b,sizeof(a))
+
+int GCD(int a, int b)
+{
+	if (b == 0)
+		return a;
+	else
+		return GCD(b, a % b);
+}
+
+const int INF = 0x3fffffff;
+const int Dic[4][2] = { {+1,+0},{-1,+0},{0,+1},{0,-1} };
+
+////////////////////////////////////////////////////////////////////////
 
 using namespace std;
 
@@ -42,7 +138,7 @@ pair<int, long long> InitMin(int node, int start, int end)
 pair<int, long long> GetMin(int node, int start, int end, int left, int right)
 {
 	if (left > end || right < start)
-		return { 1000000,1000000 };
+		return { 100000000000,100000000000 };
 
 	if (left <= start && end <= right)
 		return TreeMin[node];
@@ -90,8 +186,7 @@ pair<int, long long> Update(int node, int start, int end, int index, long long A
 	return TreeMin[node];
 }
 
-
-int main()
+int32_t main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
