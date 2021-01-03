f0c87732ca9322e70a74dcb68185127888df3e9d
MarbinSpectrum <9507ym@naver.com>
2021-01-04 07:46:05 +0900

    13977

---
+++ StudyData/백준/13977(이항 계수와 쿼리).cpp
@@ -0,0 +1,160 @@
+﻿#ifndef _GLIBCXX_NO_ASSERT 
+#include <cassert> 
+#endif #include <cctype>
+#include <cerrno> 
+#include <cfloat> 
+#include <ciso646> 
+#include <climits> 
+#include <clocale> 
+#include <cmath> 
+#include <csetjmp> 
+#include <csignal>
+#include <cstdarg> 
+#include <cstddef>
+#include <cstdio> 
+#include <cstdlib> 
+#include <cstring> 
+#include <ctime> 
+
+#if __cplusplus >= 201103L 
+#include <ccomplex> 
+#include <cfenv> 
+#include <cinttypes> 
+#include <cstdalign> 
+#include <cstdbool>
+#include <cstdint> 
+#include <ctgmath>
+#include <cwchar> 
+#include <cwctype>
+#endif 
+
+// C++ 
+#include <algorithm> 
+#include <bitset> 
+#include <complex> 
+#include <deque> 
+#include <exception> 
+#include <fstream> 
+#include <functional> 
+#include <iomanip> 
+#include <ios> 
+#include <iosfwd> 
+#include <iostream> 
+#include <istream> 
+#include <iterator> 
+#include <limits> 
+#include <list> 
+#include <locale> 
+#include <map> 
+#include <memory> 
+#include <new> 
+#include <numeric>
+#include <ostream> 
+#include <queue> 
+#include <set> 
+#include <sstream> 
+#include <stack> 
+#include <stdexcept> 
+#include <streambuf> 
+#include <string> 
+#include <typeinfo> 
+#include <utility> 
+#include <valarray> 
+#include <vector> 
+
+#if __cplusplus >= 201103L 
+#include <array> 
+#include <atomic> 
+#include <chrono> 
+#include <condition_variable> 
+#include <forward_list> 
+#include <future> 
+#include <initializer_list> 
+#include <mutex> 
+#include <random> 
+#include <ratio>
+#include <regex> 
+#include <scoped_allocator> 
+#include <system_error> 
+#include <thread> 
+#include <tuple> 
+#include <typeindex> 
+#include <type_traits> 
+#include <unordered_map> 
+#include <unordered_set> 
+#endif
+
+#pragma warning(disable:4996)
+
+using namespace std;
+
+#define endl "\n"
+#define int long long
+#define Debug(a,b) cout << a << " " << b << endl
+#define Init(a,b) memset(a,b,sizeof(a))
+
+int GCD(int a, int b)
+{
+	if (b == 0)
+		return a;
+	else
+		return GCD(b, a % b);
+}
+
+const int INF = 0x3fffffff;
+const int Dic[4][2] = { {+1,+0},{-1,+0},{0,+1},{0,-1} };
+
+////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+int F[4000001];
+int INV[4000001];
+const int MOD = 1000000007;
+
+int pw(int a, int b) {
+	int ret = 1;
+	while (b > 0) 
+	{
+		if (b % 2 != 0) 
+			ret = ret * a % MOD;
+		b /= 2;
+		a = a * a % MOD;
+	}
+	return ret;
+}
+
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	F[1] = 1;
+	for (int i = 2; i < 4000001; i++)
+		F[i] = (F[i - 1] * i) % MOD;
+	INV[4000000] = pw(F[4000000], MOD - 2);
+	for (int i = 3999999; i >= 0; i--) {
+		INV[i] = INV[i + 1] * (i + 1) % MOD;
+	}
+	int T;
+	cin >> T;
+
+	while (T--)
+	{
+		int a, b;
+		cin >> a >> b;
+		if (b == 0 || a == b)
+			cout << 1 << endl;
+		else
+		{
+			int Result = F[a];
+			Result = (Result * INV[b]) % MOD;
+			Result = (Result * INV[a - b]) % MOD;
+			cout << Result << endl;
+		}
+
+	}
+
+}
