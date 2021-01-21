02ad4dbb5b0f7f3d07afb7ea524e4be5aff52e5a
MarbinSpectrum <9507ym@naver.com>
2021-01-21 20:10:01 +0900

    10759

---
+++ StudyData/백준/10759(팰린드롬 경로 3).cpp
@@ -0,0 +1,170 @@
+﻿#ifndef _GLIBCXX_NO_ASSERT 
+#include <cassert> 
+#endif #include <cctype>
+#include <cerrno> 
+#include <cfloat> 
+#include <ciso646> 
+#include <climits> 
+#include <clocale> 
+#include <cmath> 
+#include <csetjmp> 
+#include <csignal>
+#include <cstdarg> 
+#include <cstddef>
+#include <cstdio> 
+#include <cstdlib> 
+#include <cstring> 
+#include <ctime> 
+
+#if __cplusplus >= 201103L 
+#include <ccomplex> 
+#include <cfenv> 
+#include <cinttypes> 
+#include <cstdalign> 
+#include <cstdbool>
+#include <cstdint> 
+#include <ctgmath>
+#include <cwchar> 
+#include <cwctype>
+#endif 
+
+// C++ 
+#include <algorithm> 
+#include <bitset> 
+#include <complex> 
+#include <deque> 
+#include <exception> 
+#include <fstream> 
+#include <functional> 
+#include <iomanip> 
+#include <ios> 
+#include <iosfwd> 
+#include <iostream> 
+#include <istream> 
+#include <iterator> 
+#include <limits> 
+#include <list> 
+#include <locale> 
+#include <map> 
+#include <memory> 
+#include <new> 
+#include <numeric>
+#include <ostream> 
+#include <queue> 
+#include <set> 
+#include <sstream> 
+#include <stack> 
+#include <stdexcept> 
+#include <streambuf> 
+#include <string> 
+#include <typeinfo> 
+#include <utility> 
+#include <valarray> 
+#include <vector> 
+
+#if __cplusplus >= 201103L 
+#include <array> 
+#include <atomic> 
+#include <chrono> 
+#include <condition_variable> 
+#include <forward_list> 
+#include <future> 
+#include <initializer_list> 
+#include <mutex> 
+#include <random> 
+#include <ratio>
+#include <regex> 
+#include <scoped_allocator> 
+#include <system_error> 
+#include <thread> 
+#include <tuple> 
+#include <typeindex> 
+#include <type_traits> 
+#include <unordered_map> 
+#include <unordered_set> 
+#endif
+
+#pragma warning(disable:4996)
+
+using namespace std;
+
+#define endl "\n"
+#define int long long
+#define float double
+#define Debug(a,b) cout << a << " " << b << endl
+#define Init(a,b) memset(a,b,sizeof(a))
+
+int GCD(int a, int b)
+{
+	if (b == 0)
+		return a;
+	else
+		return GCD(b, a % b);
+}
+
+int Ceil(double n)
+{
+	if (n - (int)(n) > 0)
+		return n + 1;
+	else
+		return n;
+}
+
+const int INF = 8999999999999999999;
+const int Dic[4][2] = { {+1,+0},{-1,+0},{0,+1},{0,-1} };
+
+////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+const int MOD = 1000000007;
+int N;
+char MAP[501][501];
+int DP[501][501];
+int NextDP[501][501];
+int32_t main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+
+	for (int i = 1; i <= N; i++)
+		for (int j = 1; j <= N; j++)
+			cin >> MAP[i][j];
+	for (int i = 1; i <= N; i++)
+		DP[i][i] = 1;
+
+	for (int i = 1; i <= N - 1; i++)
+	{
+		for (int r1 = 1; r1 <= N; r1++)
+			for (int r2 = N; r2 >= r1; r2--)
+			{
+				int c1 = N - r1 + 1 - i;
+				int c2 = N - r2 + 1 + i;
+				if (c1 <= 0 || c2 > N)
+					continue;
+				if (MAP[r1][c1] != MAP[r2][c2])
+					continue;
+				NextDP[r1][r2] += DP[r1][r2] % MOD;
+				NextDP[r1][r2] %= MOD;
+				NextDP[r1][r2] += DP[r1 + 1][r2] % MOD;
+				NextDP[r1][r2] %= MOD;
+				NextDP[r1][r2] += DP[r1][r2 - 1] % MOD;
+				NextDP[r1][r2] %= MOD;
+				NextDP[r1][r2] += DP[r1 + 1][r2 - 1] % MOD;
+				NextDP[r1][r2] %= MOD;
+			}
+
+		for(int a = 1; a <= N; a++)
+			for (int b = 1; b <= N; b++)
+			{
+				DP[a][b] = NextDP[a][b];
+				NextDP[a][b] = 0;
+			}
+
+	}
+
+	cout << DP[1][N] % MOD << endl;
+}
---
+++ StudyData/백준/8462(배열의 힘).cpp
@@ -0,0 +1,211 @@
+﻿#ifndef _GLIBCXX_NO_ASSERT 
+#include <cassert> 
+#endif #include <cctype>
+#include <cerrno> 
+#include <cfloat> 
+#include <ciso646> 
+#include <climits> 
+#include <clocale> 
+#include <cmath> 
+#include <csetjmp> 
+#include <csignal>
+#include <cstdarg> 
+#include <cstddef>
+#include <cstdio> 
+#include <cstdlib> 
+#include <cstring> 
+#include <ctime> 
+
+#if __cplusplus >= 201103L 
+#include <ccomplex> 
+#include <cfenv> 
+#include <cinttypes> 
+#include <cstdalign> 
+#include <cstdbool>
+#include <cstdint> 
+#include <ctgmath>
+#include <cwchar> 
+#include <cwctype>
+#endif 
+
+// C++ 
+#include <algorithm> 
+#include <bitset> 
+#include <complex> 
+#include <deque> 
+#include <exception> 
+#include <fstream> 
+#include <functional> 
+#include <iomanip> 
+#include <ios> 
+#include <iosfwd> 
+#include <iostream> 
+#include <istream> 
+#include <iterator> 
+#include <limits> 
+#include <list> 
+#include <locale> 
+#include <map> 
+#include <memory> 
+#include <new> 
+#include <numeric>
+#include <ostream> 
+#include <queue> 
+#include <set> 
+#include <sstream> 
+#include <stack> 
+#include <stdexcept> 
+#include <streambuf> 
+#include <string> 
+#include <typeinfo> 
+#include <utility> 
+#include <valarray> 
+#include <vector> 
+
+#if __cplusplus >= 201103L 
+#include <array> 
+#include <atomic> 
+#include <chrono> 
+#include <condition_variable> 
+#include <forward_list> 
+#include <future> 
+#include <initializer_list> 
+#include <mutex> 
+#include <random> 
+#include <ratio>
+#include <regex> 
+#include <scoped_allocator> 
+#include <system_error> 
+#include <thread> 
+#include <tuple> 
+#include <typeindex> 
+#include <type_traits> 
+#include <unordered_map> 
+#include <unordered_set> 
+#endif
+
+#pragma warning(disable:4996)
+
+using namespace std;
+
+#define endl "\n"
+#define int long long
+#define float double
+#define Debug(a,b) cout << a << " " << b << endl
+#define Init(a,b) memset(a,b,sizeof(a))
+
+int GCD(int a, int b)
+{
+	if (b == 0)
+		return a;
+	else
+		return GCD(b, a % b);
+}
+
+int Ceil(double n)
+{
+	if (n - (int)(n) > 0)
+		return n + 1;
+	else
+		return n;
+}
+
+const int INF = 8999999999999999999;
+const int Dic[4][2] = { {+1,+0},{-1,+0},{0,+1},{0,-1} };
+
+////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+int SqrtN = 1;
+bool cmp(const tuple<int, int, int> A, const tuple<int, int, int> B)
+{
+	int Al = get<0>(A);
+	int Ar = get<1>(A);
+	int Bl = get<0>(B);
+	int Br = get<1>(B);
+
+	if (Al / SqrtN != Bl / SqrtN)
+		return Al / SqrtN < Bl / SqrtN;
+	return Ar < Br;
+}
+
+int N;
+int A[100001];
+int M;
+vector<tuple<int, int, int>> Query;
+int Result[100001];
+int Number[1000001];
+int s = 0;
+int e = 0;
+int value = 0;
+void Count(int L, int R)
+{
+	while (s != L)
+	{
+		if (s < L)
+		{
+			value -= Number[A[s]] * Number[A[s]] * A[s];
+			Number[A[s]]--;
+			value += Number[A[s]] * Number[A[s]] * A[s];
+			s++;
+		}
+		else if (s > L)
+		{
+			value -= Number[A[s - 1]] * Number[A[s - 1]] * A[s - 1];
+			Number[A[s - 1]]++;
+			value += Number[A[s - 1]] * Number[A[s - 1]] * A[s - 1];
+			s--;
+		}
+	}
+
+	while (e != R)
+	{
+		if (e < R)
+		{
+			value -= Number[A[e + 1]] * Number[A[e + 1]] * A[e + 1];
+			Number[A[e + 1]]++;
+			value += Number[A[e + 1]] * Number[A[e + 1]] * A[e + 1];
+			e++;
+		}
+		else if (e > R)
+		{
+			value -= Number[A[e]] * Number[A[e]] * A[e];
+			Number[A[e]]--;
+			value += Number[A[e]] * Number[A[e]] * A[e];
+			e--;
+		}
+	}
+}
+int32_t main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+	cin >> M;
+	SqrtN = sqrt(N);
+	for (int i = 1; i <= N; i++)
+		cin >> A[i];
+	for (int i = 1; i <= M; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		Query.push_back(make_tuple(a, b, i));
+	}
+
+	sort(Query.begin(), Query.end(), cmp);
+
+	for (int i = 0; i < Query.size(); i++)
+	{
+		int l = get<0>(Query[i]);
+		int r = get<1>(Query[i]);
+		int a = get<2>(Query[i]);
+		Count(l, r);
+		Result[a] = value;
+	}
+
+	for (int i = 1; i <= M; i++)
+		cout << Result[i] << endl;
+}
