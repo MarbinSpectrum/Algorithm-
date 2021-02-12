663d1dc675e2ce7ea672bc8548591a274f1a9783
MarbinSpectrum <9507ym@naver.com>
2021-02-13 03:23:09 +0900

    berlekam

---
+++ StudyData/백준/10562(나이트).cpp
@@ -0,0 +1,232 @@
+﻿#ifndef _GLIBCXX_NO_ASSERT 
+#include <cassert> 
+#endif #include <cctype>
+#include <cerrno> 
+#include <cfloat> 
+#include <ciso646> 
+#include <climits> 
+#include <clocale> 
+#include <cmath> 
+#include <csetjmp> 
+#include <csignal>
+#include <cstdarg> 
+#include <cstddef>
+#include <cstdio> 
+#include <cstdlib> 
+#include <cstring> 
+#include <ctime> 
+
+#if __cplusplus >= 201103L 
+#include <ccomplex> 
+#include <cfenv> 
+#include <cinttypes> 
+#include <cstdalign> 
+#include <cstdbool>
+#include <cstdint> 
+#include <ctgmath>
+#include <cwchar> 
+#include <cwctype>
+#endif 
+
+// C++ 
+#include <algorithm> 
+#include <bitset> 
+#include <complex> 
+#include <deque> 
+#include <exception> 
+#include <fstream> 
+#include <functional> 
+#include <iomanip> 
+#include <ios> 
+#include <iosfwd> 
+#include <iostream> 
+#include <istream> 
+#include <iterator> 
+#include <limits> 
+#include <list> 
+#include <locale> 
+#include <map> 
+#include <memory> 
+#include <new> 
+#include <numeric>
+#include <ostream> 
+#include <queue> 
+#include <set> 
+#include <sstream> 
+#include <stack> 
+#include <stdexcept> 
+#include <streambuf> 
+#include <string> 
+#include <typeinfo> 
+#include <utility> 
+#include <valarray> 
+#include <vector> 
+
+#if __cplusplus >= 201103L 
+#include <array> 
+#include <atomic> 
+#include <chrono> 
+#include <condition_variable> 
+#include <forward_list> 
+#include <future> 
+#include <initializer_list> 
+#include <mutex> 
+#include <random> 
+#include <ratio>
+#include <regex> 
+#include <scoped_allocator> 
+#include <system_error> 
+#include <thread> 
+#include <tuple> 
+#include <typeindex> 
+#include <type_traits> 
+#include <unordered_map> 
+#include <unordered_set> 
+#endif
+
+#pragma warning(disable:4996)
+
+using namespace std;
+
+#define endl "\n"
+#define int long long
+#define float double
+#define Debug(a,b) cout << a << " " << b << endl
+#define Init(a,b) memset(a,b,sizeof(a))
+
+int GCD(int a, int b)
+{
+	if (b == 0)
+		return a;
+	else
+		return GCD(b, a % b);
+}
+
+int Ceil(double n)
+{
+	if (n - (int)(n) > 0)
+		return (int)n + 1;
+	else
+		return n;
+}
+
+const int INF = 8999999999999999999;
+const int Dic[4][2] = { {+1,+0},{-1,+0},{0,+1},{0,-1} };
+
+////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+const int mod = 1000000009;
+
+int ipow(int x, int p) {
+	int ret = 1, piv = x;
+	while (p) {
+		if (p & 1) ret = ret * piv % mod;
+		piv = piv * piv % mod;
+		p >>= 1;
+	}
+	return ret;
+}
+vector<int> berlekamp_massey(vector<int> x) {
+	vector<int> ls, cur;
+	int lf, ld;
+	for (int i = 0; i < x.size(); i++) {
+		int t = 0;
+		for (int j = 0; j < cur.size(); j++) {
+			t = (t + 1ll * x[i - j - 1] * cur[j]) % mod;
+		}
+		if ((t - x[i]) % mod == 0) continue;
+		if (cur.empty()) {
+			cur.resize(i + 1);
+			lf = i;
+			ld = (t - x[i]) % mod;
+			continue;
+		}
+		int k = -(x[i] - t) * ipow(ld, mod - 2) % mod;
+		vector<int> c(i - lf - 1);
+		c.push_back(k);
+		for (auto& j : ls) c.push_back(-j * k % mod);
+		if (c.size() < cur.size()) c.resize(cur.size());
+		for (int j = 0; j < cur.size(); j++) {
+			c[j] = (c[j] + cur[j]) % mod;
+		}
+		if (i - lf + (int)ls.size() >= (int)cur.size()) {
+			tie(ls, lf, ld) = make_tuple(cur, i, (t - x[i]) % mod);
+		}
+		cur = c;
+	}
+	for (auto& i : cur) i = (i % mod + mod) % mod;
+	return cur;
+}
+int get_nth(vector<int> rec, vector<int> dp, int n) {
+	int m = rec.size();
+	vector<int> s(m), t(m);
+	s[0] = 1;
+	if (m != 1) t[1] = 1;
+	else t[0] = rec[0];
+	auto mul = [&rec](vector<int> v, vector<int> w) {
+		int m = v.size();
+		vector<int> t(2 * m);
+		for (int j = 0; j < m; j++) {
+			for (int k = 0; k < m; k++) {
+				t[j + k] += 1ll * v[j] * w[k] % mod;
+				if (t[j + k] >= mod) t[j + k] -= mod;
+			}
+		}
+		for (int j = 2 * m - 1; j >= m; j--) {
+			for (int k = 1; k <= m; k++) {
+				t[j - k] += 1ll * t[j] * rec[k - 1] % mod;
+				if (t[j - k] >= mod) t[j - k] -= mod;
+			}
+		}
+		t.resize(m);
+		return t;
+	};
+	while (n) {
+		if (n & 1) s = mul(s, t);
+		t = mul(t, t);
+		n >>= 1;
+	}
+	int ret = 0;
+	for (int i = 0; i < m; i++) ret += 1ll * s[i] * dp[i] % mod;
+	return ret % mod;
+}
+int guess_nth_term(vector<int> x, int n) {
+	if (n < x.size()) return x[n];
+	vector<int> v = berlekamp_massey(x);
+	if (v.empty()) return 0;
+	return get_nth(v, x, n);
+}
+
+int DP[4][50] = 
+{
+	{ 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 73741815, 147483630, 294967260, 589934520, 179869031, 359738062, 719476124, 438952239, 877904478, 755808947, 511617885, 23235761, 46471522, 92943044, 185886088, 371772176, 743544352, 487088695, 974177390, 948354771, 896709533 },
+	{ 4, 16, 36, 81, 225, 625, 1600, 4096, 10816, 28561, 74529, 194481, 509796, 1336336, 3496900, 9150625, 23961025, 62742241, 164249856, 429981696, 125736695, 947295503, 716041218, 200652461, 886200432, 458408758, 488281642, 5232020, 529362765, 586008745, 223562643, 76425889, 19069136, 2388928, 953136135, 800450530, 539745896, 966886539, 121283871, 9235872, 533782787, 607200726, 645372102, 671380047, 668750842, 292390808, 950920440, 345351007, 557653218, 15148755 },
+	{ 8, 36, 94, 278, 1062, 3650, 11856, 39444, 135704, 456980, 1534668, 5166204, 17480600, 58888528, 198548648, 669291696, 258436230, 613387281, 676312919, 575341762, 991128221, 557546496, 284542480, 209398972, 232230803, 303596263, 939962513, 351290213, 415931359, 328520111, 887554940, 303667674, 351233655, 747600119, 130781946, 702928593, 155509746, 538853820, 548779965, 726903524, 370846848, 989333901, 795920339, 432839282, 815115627, 902444432, 3195020, 783730971, 232305131, 894592622 },
+	{ 16, 81, 278, 1365, 7164, 33858, 161307, 791722, 3859473, 18702843, 90938441, 442661923, 152542080, 466805482, 911253057, 627500238, 355979736, 651184968, 444168477, 637675570, 340713937, 193363675, 666524059, 932645942, 897647645, 834763352, 662912921, 725854997, 840822360, 61565774, 135123018, 995036230, 730107533, 462094335, 710509782, 525321589, 949550086, 8069878, 739604600, 955573146, 817055186, 27292242, 254984760, 388463753, 467535957, 483265312, 352974171, 592298092, 268922749, 66458109 }
+};
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int T;
+	cin >> T;
+	while (T--)
+	{
+		int a, b;
+		cin >> a >> b;
+		vector<int> Arr;
+		for (int i = 0; i < 50; i++)
+			Arr.push_back(DP[a-1][i]);
+
+		cout << guess_nth_term(Arr, b-1) << endl;
+
+
+
+	}
+
+}
---
+++ StudyData/백준/13430(합 구하기).cpp
@@ -0,0 +1,233 @@
+﻿#ifndef _GLIBCXX_NO_ASSERT 
+#include <cassert> 
+#endif #include <cctype>
+#include <cerrno> 
+#include <cfloat> 
+#include <ciso646> 
+#include <climits> 
+#include <clocale> 
+#include <cmath> 
+#include <csetjmp> 
+#include <csignal>
+#include <cstdarg> 
+#include <cstddef>
+#include <cstdio> 
+#include <cstdlib> 
+#include <cstring> 
+#include <ctime> 
+
+#if __cplusplus >= 201103L 
+#include <ccomplex> 
+#include <cfenv> 
+#include <cinttypes> 
+#include <cstdalign> 
+#include <cstdbool>
+#include <cstdint> 
+#include <ctgmath>
+#include <cwchar> 
+#include <cwctype>
+#endif 
+
+// C++ 
+#include <algorithm> 
+#include <bitset> 
+#include <complex> 
+#include <deque> 
+#include <exception> 
+#include <fstream> 
+#include <functional> 
+#include <iomanip> 
+#include <ios> 
+#include <iosfwd> 
+#include <iostream> 
+#include <istream> 
+#include <iterator> 
+#include <limits> 
+#include <list> 
+#include <locale> 
+#include <map> 
+#include <memory> 
+#include <new> 
+#include <numeric>
+#include <ostream> 
+#include <queue> 
+#include <set> 
+#include <sstream> 
+#include <stack> 
+#include <stdexcept> 
+#include <streambuf> 
+#include <string> 
+#include <typeinfo> 
+#include <utility> 
+#include <valarray> 
+#include <vector> 
+
+#if __cplusplus >= 201103L 
+#include <array> 
+#include <atomic> 
+#include <chrono> 
+#include <condition_variable> 
+#include <forward_list> 
+#include <future> 
+#include <initializer_list> 
+#include <mutex> 
+#include <random> 
+#include <ratio>
+#include <regex> 
+#include <scoped_allocator> 
+#include <system_error> 
+#include <thread> 
+#include <tuple> 
+#include <typeindex> 
+#include <type_traits> 
+#include <unordered_map> 
+#include <unordered_set> 
+#endif
+
+#pragma warning(disable:4996)
+
+using namespace std;
+
+#define endl "\n"
+#define int long long
+#define float double
+#define Debug(a,b) cout << a << " " << b << endl
+#define Init(a,b) memset(a,b,sizeof(a))
+
+int GCD(int a, int b)
+{
+	if (b == 0)
+		return a;
+	else
+		return GCD(b, a % b);
+}
+
+int Ceil(double n)
+{
+	if (n - (int)(n) > 0)
+		return (int)n + 1;
+	else
+		return n;
+}
+
+const int INF = 8999999999999999999;
+const int Dic[4][2] = { {+1,+0},{-1,+0},{0,+1},{0,-1} };
+
+////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+const int mod = 1000000007;
+
+int ipow(int x, int p) {
+	int ret = 1, piv = x;
+	while (p) {
+		if (p & 1) ret = ret * piv % mod;
+		piv = piv * piv % mod;
+		p >>= 1;
+	}
+	return ret;
+}
+vector<int> berlekamp_massey(vector<int> x) {
+	vector<int> ls, cur;
+	int lf, ld;
+	for (int i = 0; i < x.size(); i++) {
+		int t = 0;
+		for (int j = 0; j < cur.size(); j++) {
+			t = (t + 1ll * x[i - j - 1] * cur[j]) % mod;
+		}
+		if ((t - x[i]) % mod == 0) continue;
+		if (cur.empty()) {
+			cur.resize(i + 1);
+			lf = i;
+			ld = (t - x[i]) % mod;
+			continue;
+		}
+		int k = -(x[i] - t) * ipow(ld, mod - 2) % mod;
+		vector<int> c(i - lf - 1);
+		c.push_back(k);
+		for (auto& j : ls) c.push_back(-j * k % mod);
+		if (c.size() < cur.size()) c.resize(cur.size());
+		for (int j = 0; j < cur.size(); j++) {
+			c[j] = (c[j] + cur[j]) % mod;
+		}
+		if (i - lf + (int)ls.size() >= (int)cur.size()) {
+			tie(ls, lf, ld) = make_tuple(cur, i, (t - x[i]) % mod);
+		}
+		cur = c;
+	}
+	for (auto& i : cur) i = (i % mod + mod) % mod;
+	return cur;
+}
+int get_nth(vector<int> rec, vector<int> dp, int n) {
+	int m = rec.size();
+	vector<int> s(m), t(m);
+	s[0] = 1;
+	if (m != 1) t[1] = 1;
+	else t[0] = rec[0];
+	auto mul = [&rec](vector<int> v, vector<int> w) {
+		int m = v.size();
+		vector<int> t(2 * m);
+		for (int j = 0; j < m; j++) {
+			for (int k = 0; k < m; k++) {
+				t[j + k] += 1ll * v[j] * w[k] % mod;
+				if (t[j + k] >= mod) t[j + k] -= mod;
+			}
+		}
+		for (int j = 2 * m - 1; j >= m; j--) {
+			for (int k = 1; k <= m; k++) {
+				t[j - k] += 1ll * t[j] * rec[k - 1] % mod;
+				if (t[j - k] >= mod) t[j - k] -= mod;
+			}
+		}
+		t.resize(m);
+		return t;
+	};
+	while (n) {
+		if (n & 1) s = mul(s, t);
+		t = mul(t, t);
+		n >>= 1;
+	}
+	int ret = 0;
+	for (int i = 0; i < m; i++) ret += 1ll * s[i] * dp[i] % mod;
+	return ret % mod;
+}
+int guess_nth_term(vector<int> x, int n) {
+	if (n < x.size()) return x[n];
+	vector<int> v = berlekamp_massey(x);
+	if (v.empty()) return 0;
+	return get_nth(v, x, n);
+}
+int DP[51][301];
+int32_t main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, k;
+	cin >> k >> n;
+	for (int i = 0; i <= 50; i++)
+	{
+		if (i == 0)
+		{
+			for (int j = 1; j <= 300; j++)
+				DP[i][j] = j;
+		}
+		else
+		{
+			for (int j = 1; j <= 300; j++)
+				for (int k = 1; k <= j; k++)
+				{
+					DP[i][j] += DP[i - 1][k];
+					DP[i][j] %= mod;
+				}
+		}
+	}
+	vector<int> Arr;
+	for (int j = 1; j <= 300; j++)
+		Arr.push_back(DP[k][j]);
+
+	cout << guess_nth_term(Arr, n - 1) << endl;
+
+}
