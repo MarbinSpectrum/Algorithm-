93481bcd4eb7c1d11dd63efdb8cd198fd907f612
MarbinSpectrum <9507ym@naver.com>
2021-02-19 22:24:00 +0900

    15907

---
+++ StudyData/백준/15907(Acka의 리듬 세상).cpp
@@ -0,0 +1,162 @@
+﻿#ifndef _GLIBCXX_NO_ASSERT 
+#include <cassert> 
+#endif #include <cctype>
+#include <cerrno> 
+#include <cfloat> 
+#include <ciso646> 
+#include <climits> 
+#include <clocale> 
+#include <cmath> 
+#include <csetjmp> 
+#include <csignal>
+#include <cstdarg> 
+#include <cstddef>
+#include <cstdio> 
+#include <cstdlib> 
+#include <cstring> 
+#include <ctime> 
+
+#if __cplusplus >= 201103L 
+#include <ccomplex> 
+#include <cfenv> 
+#include <cinttypes> 
+#include <cstdalign> 
+#include <cstdbool>
+#include <cstdint> 
+#include <ctgmath>
+#include <cwchar> 
+#include <cwctype>
+#endif 
+
+// C++ 
+#include <algorithm> 
+#include <bitset> 
+#include <complex> 
+#include <deque> 
+#include <exception> 
+#include <fstream> 
+#include <functional> 
+#include <iomanip> 
+#include <ios> 
+#include <iosfwd> 
+#include <iostream> 
+#include <istream> 
+#include <iterator> 
+#include <limits> 
+#include <list> 
+#include <locale> 
+#include <map> 
+#include <memory> 
+#include <new> 
+#include <numeric>
+#include <ostream> 
+#include <queue> 
+#include <set> 
+#include <sstream> 
+#include <stack> 
+#include <stdexcept> 
+#include <streambuf> 
+#include <string> 
+#include <typeinfo> 
+#include <utility> 
+#include <valarray> 
+#include <vector> 
+
+#if __cplusplus >= 201103L 
+#include <array> 
+#include <atomic> 
+#include <chrono> 
+#include <condition_variable> 
+#include <forward_list> 
+#include <future> 
+#include <initializer_list> 
+#include <mutex> 
+#include <random> 
+#include <ratio>
+#include <regex> 
+#include <scoped_allocator> 
+#include <system_error> 
+#include <thread> 
+#include <tuple> 
+#include <typeindex> 
+#include <type_traits> 
+#include <unordered_map> 
+#include <unordered_set> 
+#endif
+
+#pragma warning(disable:4996)
+
+using namespace std;
+
+#define endl "\n"
+#define int long long
+#define float double
+#define Debug(a,b) cout << a << " " << b << endl
+#define Init(a,b) memset(a,b,sizeof(a))
+
+int GCD(int a, int b)
+{
+    if (b == 0)
+        return a;
+    else
+        return GCD(b, a % b);
+}
+
+int Ceil(double n)
+{
+    if (n - (int)(n) > 0)
+        return (int)n + 1;
+    else
+        return n;
+}
+
+const int INF = 8999999999999999999;
+const int Dic[4][2] = { {+1,+0},{-1,+0},{0,+1},{0,-1} };
+
+////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+vector<int> Prime;
+int N;
+int Arr[3000001];
+bool PrimeCheck[2000001];
+int Check[3000001];
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    for (int i = 2; i <= 1000000; i++) 
+    {
+        if (!PrimeCheck[i])
+            Prime.push_back(i);
+        for (int j = 0; j < Prime.size() && i * Prime[j] < 1000000; j++) 
+        {
+            PrimeCheck[i * Prime[j]] = true;
+            if (i % Prime[j] == false)
+                break;
+        }
+    }
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+        cin >> Arr[i];
+
+    int result = 0;
+    for (int i = 0; i < Prime.size(); i++)
+    {
+        int primeNum = Prime[i];
+
+        for (int j = 0; j < N; j++)
+        {
+            Check[Arr[j] % primeNum]++;
+            result = max(result, Check[Arr[j] % primeNum]);
+        }
+        for (int j = 0; j < N; j++)
+            Check[Arr[j] % primeNum]--;
+    }
+    cout << result << endl;
+}
