0787207c855c0870f9aadf4fb79feebb59d30f1e
MarbinSpectrum <9507ym@naver.com>
2021-03-06 21:07:40 +0900

    4384

---
+++ StudyData/백준/4384(공평하게 팀 나누기).cpp
@@ -0,0 +1,166 @@
+﻿#ifndef _GLIBCXX_NO_ASSERT 
+#include <cassert> 
+#endif #include <cctype>
+#include <cerrno> 
+#include <cfloat> 
+#include <ciso646> 
+#include <climits> 
+#include <clocale> 
+#include <cmath> 
+#include <csetjmp> 
+#include <csignal>
+#include <cstdarg> 
+#include <cstddef>
+#include <cstdio> 
+#include <cstdlib> 
+#include <cstring> 
+#include <ctime> 
+
+#if __cplusplus >= 201103L 
+#include <ccomplex> 
+#include <cfenv> 
+#include <cinttypes> 
+#include <cstdalign> 
+#include <cstdbool>
+#include <cstdint> 
+#include <ctgmath>
+#include <cwchar> 
+#include <cwctype>
+#endif 
+//
+//// C++ 
+#include <algorithm> 
+#include <bitset> 
+#include <complex> 
+#include <deque> 
+#include <exception> 
+#include <fstream> 
+#include <functional> 
+#include <iomanip> 
+#include <ios> 
+#include <iosfwd> 
+#include <iostream> 
+#include <istream> 
+#include <iterator> 
+#include <limits> 
+#include <list> 
+#include <locale> 
+#include <map> 
+#include <memory> 
+#include <new> 
+#include <numeric>
+#include <ostream> 
+#include <queue> 
+#include <set> 
+#include <sstream> 
+#include <stack> 
+#include <stdexcept> 
+#include <streambuf> 
+#include <string> 
+#include <typeinfo> 
+#include <utility> 
+#include <valarray> 
+#include <vector> 
+//
+#if __cplusplus >= 201103L 
+#include <array> 
+#include <atomic> 
+#include <chrono> 
+#include <condition_variable> 
+#include <forward_list> 
+#include <future> 
+#include <initializer_list> 
+#include <mutex> 
+#include <random> 
+#include <ratio>
+#include <regex> 
+#include <scoped_allocator> 
+#include <system_error> 
+#include <thread> 
+#include <tuple> 
+#include <typeindex> 
+#include <type_traits> 
+#include <unordered_map> 
+#include <unordered_set> 
+#endif
+
+#pragma warning(disable:4996)
+
+using namespace std;
+
+#define endl "\n"
+#define int long long
+#define float double
+#define Debug(a,b) cout << a << " " << b << endl
+#define Init(a,b) memset(a,b,sizeof(a))
+
+int GCD(int a, int b)
+{
+    if (b == 0)
+        return a;
+    else
+        return GCD(b, a % b);
+}
+
+int Ceil(double n)
+{
+    if (n - (int)(n) > 0)
+        return (int)n + 1;
+    else
+        return n;
+}
+
+const int INF = 8999999999999999999;
+const int Dic[4][2] = { {+1,+0},{-1,+0},{0,+1},{0,-1} };
+
+////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+int N;
+int K[101];
+//DP[선수수][첫번째팀합]
+bool DP[51][50000];
+int Sum = 0;
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> K[i];
+        Sum += K[i];
+    }
+
+    for (int j = 0; j < 51; j++)
+        for (int k = 0; k < 50000; k++)
+            DP[j][k] = false;
+
+    DP[0][0] = true;
+    for (int i = 0; i < N; i++)
+        for (int j = N/2; j >= 1; j--)
+            for (int k = 50000; k >= K[i]; k--)
+                DP[j][k] = DP[j][k] || DP[j - 1][k - K[i]];
+
+    int dis = INF;
+    pair<int, int> Result = { 0,0 };
+
+    for (int k = 0; k < 50000; k++)
+    {
+        if (DP[N / 2][k])
+        {
+            int a = k;
+            int b = Sum - a;
+            if (dis > abs(a - b))
+            {
+                dis = abs(a - b);
+                Result = { a,b };
+            }
+        }
+    }
+
+    Debug(min(Result.first, Result.second), max(Result.first, Result.second));
+}
