3ab8c8e04068023568cbda5261d32f077250fd32
MarbinSpectrum <9507ym@naver.com>
2021-05-11 23:48:46 +0900

    15927

---
+++ StudyData/백준/15927(회문은 회문아니야!!).cpp
@@ -0,0 +1,182 @@
+#ifndef _GLIBCXX_NO_ASSERT 
+#include <cassert> 
+#endif #include <cctype>
+#include <cerrno> 
+#include <cfloat> 
+#include <ciso646> 
+#include <climits> 
+#include <clocale> 
+#include <cmath> 
+#include <csetjmp> 
+#include <csignal>
+#include <cstdarg> 
+#include <cstddef>
+#include <cstdio> 
+#include <cstdlib> 
+#include <cstring> 
+#include <ctime> 
+
+#if __cplusplus >= 201103L 
+#include <ccomplex> 
+#include <cfenv> 
+#include <cinttypes> 
+#include <cstdalign> 
+#include <cstdbool>
+#include <cstdint> 
+#include <ctgmath>
+#include <cwchar> 
+#include <cwctype>
+#endif 
+//
+//// C++ 
+#include <algorithm> 
+#include <bitset> 
+#include <complex> 
+#include <deque> 
+#include <exception> 
+#include <fstream> 
+#include <functional> 
+#include <iomanip> 
+#include <ios> 
+#include <iosfwd> 
+#include <iostream> 
+#include <istream> 
+#include <iterator> 
+#include <limits> 
+#include <list> 
+#include <locale> 
+#include <map> 
+#include <memory> 
+#include <new> 
+#include <numeric>
+#include <ostream> 
+#include <queue> 
+#include <set> 
+#include <sstream> 
+#include <stack> 
+#include <stdexcept> 
+#include <streambuf> 
+#include <string> 
+#include <typeinfo> 
+#include <utility> 
+#include <valarray> 
+#include <vector> 
+//
+#if __cplusplus >= 201103L 
+#include <array> 
+#include <atomic> 
+#include <chrono> 
+#include <condition_variable> 
+#include <forward_list> 
+#include <future> 
+#include <initializer_list> 
+#include <mutex> 
+#include <random> 
+#include <ratio>
+#include <regex> 
+#include <scoped_allocator> 
+#include <system_error> 
+#include <thread> 
+#include <tuple> 
+#include <typeindex> 
+#include <type_traits> 
+#include <unordered_map> 
+#include <unordered_set> 
+#endif
+
+#pragma warning(disable:4996)
+
+using namespace std;
+
+
+#define endl "\n"
+#define int long long
+#define float double
+#define Debug(a,b) cout << a << " " << b << endl
+
+
+int GCD(int a, int b)
+{
+    if (b == 0)
+        return a;
+    else
+        return GCD(b, a % b);
+}
+
+int LCM(int a, int b)
+{
+    return a * b / GCD(a, b);
+}
+
+int Ceil(double n)
+{
+    if (n - (int)(n) > 0)
+        return (int)n + 1;
+    else
+        return n;
+}
+
+int Pow(int a, int b)
+{
+    if (b == 0)
+        return 1;
+    if (b == 1)
+        return a;
+    int temp = Pow(a, b / 2);
+
+    return temp * temp * a * (b % 2);
+}
+
+const int INF = 8999999999999999999;
+const int Dic[4][2] = { {+1,+0},{-1,+0},{0,+1},{0,-1} };
+
+////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+string s;
+
+bool isPalindrome(string st)
+{
+    int l = 0;
+    int r = st.size() - 1;
+    while (l <= r)
+        if (st[l++] != st[r--])
+            return false;
+    return true;
+}
+bool allSame(string st)
+{
+    char check = st[0];
+    for (int i = 1; i < st.size(); i++)
+        if (st[i] != check)
+            return false;
+    return true;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> s;
+
+    if (allSame(s))
+    {
+        cout << -1 << endl;
+        return 0;
+    }
+
+    if (isPalindrome(s))
+    {
+        cout << s.size() - 1 << endl;
+        return 0;
+    }
+    else
+    {
+        cout << s.size() << endl;
+        return 0;
+    }
+
+
+}
