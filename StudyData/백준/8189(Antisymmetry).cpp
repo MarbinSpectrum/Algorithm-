2dd21e162f6b13556110dc82749e8638451474ef
MarbinSpectrum <9507ym@naver.com>
2021-06-06 01:23:42 +0900

    8189

---
+++ StudyData/백준/8189(Antisymmetry).cpp
@@ -0,0 +1,192 @@
+#ifndef _GLIBCXX_NO_ASSERT 
+#include <cassert> 
+#endif #include <cctype>
+#include <cerrno> 
+#include <cfloat> 
+#include <ciso646> 
+#include <climits> 
+#include <clocale> 
+#include <cmath> 
+#include <csetjmp> 
+#include <csignal>
+#include <cstdarg> 
+#include <cstddef>
+#include <cstdio> 
+#include <cstdlib> 
+#include <cstring> 
+#include <ctime> 
+
+#if __cplusplus >= 201103L 
+#include <ccomplex> 
+#include <cfenv> 
+#include <cinttypes> 
+#include <cstdalign> 
+#include <cstdbool>
+#include <cstdint> 
+#include <ctgmath>
+#include <cwchar> 
+#include <cwctype>
+#endif 
+//
+//// C++ 
+#include <algorithm> 
+#include <bitset> 
+#include <complex> 
+#include <deque> 
+#include <exception> 
+#include <fstream> 
+#include <functional> 
+#include <iomanip> 
+#include <ios> 
+#include <iosfwd> 
+#include <iostream> 
+#include <istream> 
+#include <iterator> 
+#include <limits> 
+#include <list> 
+#include <locale> 
+#include <map> 
+#include <memory> 
+#include <new> 
+#include <numeric>
+#include <ostream> 
+#include <queue> 
+#include <set> 
+#include <sstream> 
+#include <stack> 
+#include <stdexcept> 
+#include <streambuf> 
+#include <string> 
+#include <typeinfo> 
+#include <utility> 
+#include <valarray> 
+#include <vector> 
+//
+#if __cplusplus >= 201103L 
+#include <array> 
+#include <atomic> 
+#include <chrono> 
+#include <condition_variable> 
+#include <forward_list> 
+#include <future> 
+#include <initializer_list> 
+#include <mutex> 
+#include <random> 
+#include <ratio>
+#include <regex> 
+#include <scoped_allocator> 
+#include <system_error> 
+#include <thread> 
+#include <tuple> 
+#include <typeindex> 
+#include <type_traits> 
+#include <unordered_map> 
+#include <unordered_set> 
+#endif
+
+#pragma warning(disable:4996)
+
+using namespace std;
+
+
+#define endl "\n"
+#define int long long
+#define float double
+#define Debug(a,b) cout << a << " " << b << endl
+
+
+int GCD(int a, int b)
+{
+	if (b == 0)
+		return a;
+	else
+		return GCD(b, a % b);
+}
+
+int LCM(int a, int b)
+{
+	return a * b / GCD(a, b);
+}
+
+int Ceil(double n)
+{
+	if (n - (int)(n) > 0)
+		return (int)n + 1;
+	else
+		return n;
+}
+
+int Pow(int a, int b)
+{
+	if (b == 0)
+		return 1;
+	if (b == 1)
+		return a;
+	int temp = Pow(a, b / 2);
+	if (b % 2 == 0)
+		return temp * temp;
+	else
+		return temp * temp * a;
+}
+
+const int INF = 0x3f3f3f3f3f3f3f3fLL;
+const int Dic[4][2] = { {+1,+0},{-1,+0},{0,+1},{0,-1} };
+
+////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+int A[1000001];
+
+void manachers(string S)
+{
+	int r = 0, p = 0;
+	for (int i = 0; i < S.length(); i++)
+	{
+		if (S[i] != '#')
+			continue;
+		if (i <= r)
+			A[i] = min(A[2 * p - i], r - i);
+		else
+			A[i] = 0;
+
+		while (i - A[i] - 1 >= 0 && i + A[i] + 1 < S.length() &&
+			((S[i - A[i] - 1] != S[i + A[i] + 1] && S[i - A[i] - 1] != '#' && S[i + A[i] + 1] != '#') ||
+				S[i - A[i] - 1] == '#' && S[i + A[i] + 1] == '#'))
+			A[i]++;
+
+		if (r < i + A[i])
+		{
+			r = i + A[i];
+			p = i;
+		}
+	}
+}
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	cin >> N;
+	string s;
+	cin >> s;
+	string ns = "#";
+	for (int i = 0; i < s.length(); i++)
+	{
+		ns += s[i];
+		ns += "#";
+	}
+
+	manachers(ns);
+
+	int ans = 0;
+	for (int i = 0; i < ns.length(); i++)
+		if (ns[i] == '#')
+			ans += A[i] / 2;
+
+	cout << ans << endl;
+
+}
