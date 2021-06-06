089b7ff55f2b88bb2b53bdd7d001d3f15c21aec1
MarbinSpectrum <9507ym@naver.com>
2021-06-06 23:24:51 +0900

    1214

---
+++ StudyData/백준/1214(쿨한 물건 구매).cpp
@@ -0,0 +1,181 @@
+#ifndef _GLIBCXX_NO_ASSERT 
+#include <cassert> 
+#endif #include <cctype>
+#include <cerrno> 
+#include <cfloat> 
+#include <ciso646> 
+#include <climits> 
+#include <clocale> 
+#include <cmath> 
+#include <csetjmp> 
+#include <csignal>
+#include <cstdarg> 
+#include <cstddef>
+#include <cstdio> 
+#include <cstdlib> 
+#include <cstring> 
+#include <ctime> 
+
+#if __cplusplus >= 201103L 
+#include <ccomplex> 
+#include <cfenv> 
+#include <cinttypes> 
+#include <cstdalign> 
+#include <cstdbool>
+#include <cstdint> 
+#include <ctgmath>
+#include <cwchar> 
+#include <cwctype>
+#endif 
+//
+//// C++ 
+#include <algorithm> 
+#include <bitset> 
+#include <complex> 
+#include <deque> 
+#include <exception> 
+#include <fstream> 
+#include <functional> 
+#include <iomanip> 
+#include <ios> 
+#include <iosfwd> 
+#include <iostream> 
+#include <istream> 
+#include <iterator> 
+#include <limits> 
+#include <list> 
+#include <locale> 
+#include <map> 
+#include <memory> 
+#include <new> 
+#include <numeric>
+#include <ostream> 
+#include <queue> 
+#include <set> 
+#include <sstream> 
+#include <stack> 
+#include <stdexcept> 
+#include <streambuf> 
+#include <string> 
+#include <typeinfo> 
+#include <utility> 
+#include <valarray> 
+#include <vector> 
+//
+#if __cplusplus >= 201103L 
+#include <array> 
+#include <atomic> 
+#include <chrono> 
+#include <condition_variable> 
+#include <forward_list> 
+#include <future> 
+#include <initializer_list> 
+#include <mutex> 
+#include <random> 
+#include <ratio>
+#include <regex> 
+#include <scoped_allocator> 
+#include <system_error> 
+#include <thread> 
+#include <tuple> 
+#include <typeindex> 
+#include <type_traits> 
+#include <unordered_map> 
+#include <unordered_set> 
+#endif
+
+#pragma warning(disable:4996)
+
+using namespace std;
+
+
+#define endl "\n"
+#define int long long
+#define float double
+#define Debug(a,b) cout << a << " " << b << endl
+
+
+int GCD(int a, int b)
+{
+	if (b == 0)
+		return a;
+	else
+		return GCD(b, a % b);
+}
+
+int LCM(int a, int b)
+{
+	return a * b / GCD(a, b);
+}
+
+int Ceil(double n)
+{
+	if (n - (int)(n) > 0)
+		return (int)n + 1;
+	else
+		return n;
+}
+
+int Pow(int a, int b)
+{
+	if (b == 0)
+		return 1;
+	if (b == 1)
+		return a;
+	int temp = Pow(a, b / 2);
+	if (b % 2 == 0)
+		return temp * temp;
+	else
+		return temp * temp * a;
+}
+
+const int INF = 0x3f3f3f3f3f3f3f3fLL;
+const int Dic[4][2] = { {+1,+0},{-1,+0},{0,+1},{0,-1} };
+
+////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+set<int> SET;
+
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int D, P, Q;
+
+	cin >> D >> P >> Q;
+	if (Q > P)
+		swap(P, Q);
+
+	int A = Ceil((float)D / (float)P);
+
+	int ans = 1000000000000;
+
+	for (int i = A; i >= 0; i--)
+	{
+		if (i != A)
+		{
+			int S = (D - i * P) % Q;
+			if (SET.find(S) == SET.end())
+				SET.insert(S);
+			else
+				break;
+
+			int temp = i * P + Ceil((float)(D - i * P) / (float)Q) * Q;
+			if (abs(ans - D) > abs(temp - D))
+				ans = temp;
+		}
+		else
+		{
+			int temp = i * P;
+			if (abs(ans - D) > abs(temp - D))
+				ans = temp;
+		}
+	}
+
+	cout << ans << endl;
+}
