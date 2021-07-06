ef6c848ad5782c80d7806da5d0a3a3a438a031b3
MarbinSpectrum <9507ym@naver.com>
2021-07-07 00:17:57 +0900

    17022

---
+++ StudyData/백준/17022(Sleepy Cow Sorting).cpp
@@ -0,0 +1,205 @@
+#ifndef _GLIBCXX_NO_ASSERT 
+#include <cassert> 
+#endif #include <cctype>
+#include <cerrno> 
+#include <cfloat> 
+#include <ciso646> 
+#include <climits> 
+#include <clocale> 
+#include <cmath> 
+#include <csetjmp> 
+#include <csignal>
+#include <cstdarg> 
+#include <cstddef>
+#include <cstdio> 
+#include <cstdlib> 
+#include <cstring> 
+#include <ctime> 
+
+#if __cplusplus >= 201103L 
+#include <ccomplex> 
+#include <cfenv> 
+#include <cinttypes> 
+#include <cstdalign> 
+#include <cstdbool>
+#include <cstdint> 
+#include <ctgmath>
+#include <cwchar> 
+#include <cwctype>
+#endif 
+//
+//// C++ 
+#include <algorithm> 
+#include <bitset> 
+#include <complex> 
+#include <deque> 
+#include <exception> 
+#include <fstream> 
+#include <functional> 
+#include <iomanip> 
+#include <ios> 
+#include <iosfwd> 
+#include <iostream> 
+#include <istream> 
+#include <iterator> 
+#include <limits> 
+#include <list> 
+#include <locale> 
+#include <map> 
+#include <memory> 
+#include <new> 
+#include <numeric>
+#include <ostream> 
+#include <queue> 
+#include <set> 
+#include <sstream> 
+#include <stack> 
+#include <stdexcept> 
+#include <streambuf> 
+#include <string> 
+#include <typeinfo> 
+#include <utility> 
+#include <valarray> 
+#include <vector> 
+//
+#if __cplusplus >= 201103L 
+#include <array> 
+#include <atomic> 
+#include <chrono> 
+#include <condition_variable> 
+#include <forward_list> 
+#include <future> 
+#include <initializer_list> 
+#include <mutex> 
+#include <random> 
+#include <ratio>
+#include <regex> 
+#include <scoped_allocator> 
+#include <system_error> 
+#include <thread> 
+#include <tuple> 
+#include <typeindex> 
+#include <type_traits> 
+#include <unordered_map> 
+#include <unordered_set> 
+#endif
+
+#pragma warning(disable:4996)
+
+using namespace std;
+
+
+#define endl "\n"
+#define int long long
+#define float double
+#define Debug(a,b) cout << a << " " << b << endl
+
+
+int GCD(int a, int b)
+{
+    if (b == 0)
+        return a;
+    else
+        return GCD(b, a % b);
+}
+
+int LCM(int a, int b)
+{
+    return a * b / GCD(a, b);
+}
+
+int Ceil(double n)
+{
+    if (n - (int)(n) > 0)
+        return (int)n + 1;
+    else
+        return n;
+}
+
+int Pow(int a, int b)
+{
+    if (b == 0)
+        return 1;
+    if (b == 1)
+        return a;
+    int temp = Pow(a, b / 2);
+
+    return temp * temp * ((b % 2) ? 1 : a);
+}
+
+const int INF = 8999999999999999999;
+const int Dic[4][2] = { {+1,+0},{-1,+0},{0,+1},{0,-1} };
+
+////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+int N;
+deque<int> Arr;
+
+int tree[100001];
+int Sum(int i)
+{
+    int ans = 0;
+    while (i > 0)
+    {
+        ans += tree[i];
+        i -= (i & -i);
+    }
+    return ans;
+}
+
+void Update(int i, int num)
+{
+    while (i <= N)
+    {
+        tree[i] += num;
+        i += (i & -i);
+    }
+}
+
+vector<int> Ans;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        int a;
+        cin >> a;
+        Arr.push_back(a);
+    }
+
+    int BACK = Arr.back();
+    Update(Arr.back(), 1);
+    Arr.pop_back();
+
+    while (!Arr.empty() && Arr.back() < BACK)
+    {
+        BACK = Arr.back();
+        Update(Arr.back(), 1);
+        Arr.pop_back();
+    }
+
+    while (!Arr.empty())
+    {
+        int V = Sum(Arr.front());
+        Update(Arr.front(), 1);
+        Arr.pop_front();
+
+        V += Arr.size();
+        if (V > 0)
+            Ans.push_back(V);
+    }
+
+    cout << Ans.size() << endl;
+    for (int i = 0; i < Ans.size(); i++)
+        cout << Ans[i] << " ";
+    cout << endl;
+
+    return 0;
+}
