7d2aa0308716046e69aee402569d291cc50b203e
MarbinSpectrum <9507ym@naver.com>
2021-08-04 14:11:21 +0900

    14459

---
+++ StudyData/백준/14459(소가 길을 건너간 이유 11).cpp
@@ -0,0 +1,191 @@
+#ifndef _GLIBCXX_NO_ASSERT 
+#include <cassert> 
+#endif #include <cctype>
+#include <cerrno> 
+#include <cfloat> 
+#include <ciso646> 
+#include <climits> 
+#include <clocale> 
+#include <cmath> 
+#include <csetjmp> 
+#include <csignal>
+#include <cstdarg> 
+#include <cstddef>
+#include <cstdio> 
+#include <cstdlib> 
+#include <cstring> 
+#include <ctime> 
+
+#if __cplusplus >= 201103L 
+#include <ccomplex> 
+#include <cfenv> 
+#include <cinttypes> 
+#include <cstdalign> 
+#include <cstdbool>
+#include <cstdint> 
+#include <ctgmath>
+#include <cwchar> 
+#include <cwctype>
+#endif 
+//
+//// C++ 
+#include <algorithm> 
+#include <bitset> 
+#include <complex> 
+#include <deque> 
+#include <exception> 
+#include <fstream> 
+#include <functional> 
+#include <iomanip> 
+#include <ios> 
+#include <iosfwd> 
+#include <iostream> 
+#include <istream> 
+#include <iterator> 
+#include <limits> 
+#include <list> 
+#include <locale> 
+#include <map> 
+#include <memory> 
+#include <new> 
+#include <numeric>
+#include <ostream> 
+#include <queue> 
+#include <set> 
+#include <sstream> 
+#include <stack> 
+#include <stdexcept> 
+#include <streambuf> 
+#include <string> 
+#include <typeinfo> 
+#include <utility> 
+#include <valarray> 
+#include <vector> 
+//
+#if __cplusplus >= 201103L 
+#include <array> 
+#include <atomic> 
+#include <chrono> 
+#include <condition_variable> 
+#include <forward_list> 
+#include <future> 
+#include <initializer_list> 
+#include <mutex> 
+#include <random> 
+#include <ratio>
+#include <regex> 
+#include <scoped_allocator> 
+#include <system_error> 
+#include <thread> 
+#include <tuple> 
+#include <typeindex> 
+#include <type_traits> 
+#include <unordered_map> 
+#include <unordered_set> 
+#endif
+
+#pragma warning(disable:4996)
+
+using namespace std;
+
+
+#define endl "\n"
+#define int long long
+#define float double
+#define Debug(a,b) cout << a << " " << b << endl
+
+
+int GCD(int a, int b)
+{
+    if (b == 0)
+        return a;
+    else
+        return GCD(b, a % b);
+}
+
+int LCM(int a, int b)
+{
+    return a * b / GCD(a, b);
+}
+
+int Ceil(double n)
+{
+    if (n - (int)(n) > 0)
+        return (int)n + 1;
+    else
+        return n;
+}
+
+int Pow(int a, int b)
+{
+    if (b == 0)
+        return 1;
+    if (b == 1)
+        return a;
+    int temp = Pow(a, b / 2);
+
+    return temp * temp * ((b % 2) ? a : 1);
+}
+
+const int INF = 8999999999999999999;
+const int Dic[4][2] = { {+1,+0},{-1,+0},{0,+1},{0,-1} };
+
+////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+
+int N;
+vector<pair<int, int>> Arr;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+	cin >> N;
+	vector<pair<int,int>> F;
+	map<int, int> MAP;
+	for (int i = 0; i < N; i++)
+	{
+		int a;
+		cin >> a;
+		F.push_back({ i,a });
+	}
+	for (int i = 0; i < N; i++)
+	{
+		int a;
+		cin >> a;
+		MAP[a] = i;
+	}
+	for (int i = 0; i < F.size(); i++)
+	{
+		int now = F[i].second;
+		for (int j = max((int)1, now - 4); j <= min(N, now + 4); j++)
+			Arr.push_back({ i,-MAP[j] });
+	}
+
+	sort(Arr.begin(), Arr.end());
+	for (int i = 0; i < Arr.size(); i++)
+		Arr[i].second = -Arr[i].second;
+
+	vector<int> lis(1, -1);
+	int ans = 0;
+	for (int i = 0; i < Arr.size(); i++)
+	{
+		if (lis.back() < Arr[i].second)
+		{
+			ans++;
+			lis.push_back(Arr[i].second);
+		}
+		else
+		{
+			int it = lower_bound(lis.begin(), lis.end(), Arr[i].second) - lis.begin();
+			lis[it] = Arr[i].second;
+		}
+	}
+
+	cout << ans << endl;
+
+}
