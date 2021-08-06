7d5687acc40a3f3853d27c1048e7a2556e27c61c
MarbinSpectrum <9507ym@naver.com>
2021-08-06 13:19:47 +0900

    18120

---
+++ StudyData/백준/18120(대농부 김상혁).cpp
@@ -0,0 +1,176 @@
+#ifndef _GLIBCXX_NO_ASSERT 
+#include <cassert> 
+#endif #include <cctype>
+#include <cerrno> 
+#include <cfloat> 
+#include <ciso646> 
+#include <climits> 
+#include <clocale> 
+#include <cmath> 
+#include <csetjmp> 
+#include <csignal>
+#include <cstdarg> 
+#include <cstddef>
+#include <cstdio> 
+#include <cstdlib> 
+#include <cstring> 
+#include <ctime> 
+
+#if __cplusplus >= 201103L 
+#include <ccomplex> 
+#include <cfenv> 
+#include <cinttypes> 
+#include <cstdalign> 
+#include <cstdbool>
+#include <cstdint> 
+#include <ctgmath>
+#include <cwchar> 
+#include <cwctype>
+#endif 
+//
+//// C++ 
+#include <algorithm> 
+#include <bitset> 
+#include <complex> 
+#include <deque> 
+#include <exception> 
+#include <fstream> 
+#include <functional> 
+#include <iomanip> 
+#include <ios> 
+#include <iosfwd> 
+#include <iostream> 
+#include <istream> 
+#include <iterator> 
+#include <limits> 
+#include <list> 
+#include <locale> 
+#include <map> 
+#include <memory> 
+#include <new> 
+#include <numeric>
+#include <ostream> 
+#include <queue> 
+#include <set> 
+#include <sstream> 
+#include <stack> 
+#include <stdexcept> 
+#include <streambuf> 
+#include <string> 
+#include <typeinfo> 
+#include <utility> 
+#include <valarray> 
+#include <vector> 
+//
+#if __cplusplus >= 201103L 
+#include <array> 
+#include <atomic> 
+#include <chrono> 
+#include <condition_variable> 
+#include <forward_list> 
+#include <future> 
+#include <initializer_list> 
+#include <mutex> 
+#include <random> 
+#include <ratio>
+#include <regex> 
+#include <scoped_allocator> 
+#include <system_error> 
+#include <thread> 
+#include <tuple> 
+#include <typeindex> 
+#include <type_traits> 
+#include <unordered_map> 
+#include <unordered_set> 
+#endif
+
+#pragma warning(disable:4996)
+
+using namespace std;
+
+
+#define endl "\n"
+#define int long long
+#define float double
+#define Debug(a,b) cout << a << " " << b << endl
+
+
+int GCD(int a, int b)
+{
+    if (b == 0)
+        return a;
+    else
+        return GCD(b, a % b);
+}
+
+int LCM(int a, int b)
+{
+    return a * b / GCD(a, b);
+}
+
+int Ceil(double n)
+{
+    if (n - (int)(n) > 0)
+        return (int)n + 1;
+    else
+        return n;
+}
+
+int Pow(int a, int b)
+{
+    if (b == 0)
+        return 1;
+    if (b == 1)
+        return a;
+    int temp = Pow(a, b / 2);
+
+    return temp * temp * ((b % 2) ? a : 1);
+}
+
+const int INF = 8999999999999999999;
+const int Dic[4][2] = { {+1,+0},{-1,+0},{0,+1},{0,-1} };
+
+////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+float N, A;
+float a, b, c;
+vector<pair<float, float>> Arr;
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cout << fixed;
+    cout.precision(7);
+
+    cin >> N >> A;
+
+    for (int i = 0; i < N; i++)
+    {
+        int x, y, w;
+        cin >> x >> y >> w;
+        float dis = sqrt(x * x + y * y);
+        Arr.push_back({ dis,w });
+    }
+    sort(Arr.begin(), Arr.end());   
+
+    a = b = c = 0;
+    a = -A;
+    float ans = 0;
+    for (int i = 0; i < N; i++)
+    {
+        float dis = Arr[i].first;
+        float w = Arr[i].second;
+        b += w;
+        c += w * -dis;
+        float r = -b / (2.0f * a);
+
+        float ans2 = r * r * a + b * r + c;
+
+        ans = max(ans, ans2);
+    }
+    cout << ans << endl;
+}
