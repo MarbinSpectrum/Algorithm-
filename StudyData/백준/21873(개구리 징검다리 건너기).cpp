99f8f9740dc26ab3de1a6c357dcad8d339be6b59
MarbinSpectrum <9507ym@naver.com>
2021-09-03 20:19:00 +0900

    21873

---
+++ StudyData/백준/21873(개구리 징검다리 건너기).cpp
@@ -0,0 +1,213 @@
+#ifndef _GLIBCXX_NO_ASSERT 
+#include <cassert> 
+#endif #include <cctype>
+#include <cerrno> 
+#include <cfloat> 
+#include <ciso646> 
+#include <climits> 
+#include <clocale> 
+#include <cmath> 
+#include <csetjmp> 
+#include <csignal>
+#include <cstdarg> 
+#include <cstddef>
+#include <cstdio> 
+#include <cstdlib> 
+#include <cstring> 
+#include <ctime> 
+
+#if __cplusplus >= 201103L 
+#include <ccomplex> 
+#include <cfenv> 
+#include <cinttypes> 
+#include <cstdalign> 
+#include <cstdbool>
+#include <cstdint> 
+#include <ctgmath>
+#include <cwchar> 
+#include <cwctype>
+#endif 
+//
+//// C++ 
+#include <algorithm> 
+#include <bitset> 
+#include <complex> 
+#include <deque> 
+#include <exception> 
+#include <fstream> 
+#include <functional> 
+#include <iomanip> 
+#include <ios> 
+#include <iosfwd> 
+#include <iostream> 
+#include <istream> 
+#include <iterator> 
+#include <limits> 
+#include <list> 
+#include <locale> 
+#include <map> 
+#include <memory> 
+#include <new> 
+#include <numeric>
+#include <ostream> 
+#include <queue> 
+#include <set> 
+#include <sstream> 
+#include <stack> 
+#include <stdexcept> 
+#include <streambuf> 
+#include <string> 
+#include <typeinfo> 
+#include <utility> 
+#include <valarray> 
+#include <vector> 
+#include <unordered_map> 
+//
+#if __cplusplus >= 201103L 
+#include <array> 
+#include <atomic> 
+#include <chrono> 
+#include <condition_variable> 
+#include <forward_list> 
+#include <future> 
+#include <initializer_list> 
+#include <mutex> 
+#include <random> 
+#include <ratio>
+#include <regex> 
+#include <scoped_allocator> 
+#include <system_error> 
+#include <thread> 
+#include <tuple> 
+#include <typeindex> 
+#include <type_traits> 
+#include <unordered_map> 
+#include <unordered_set> 
+#endif
+
+#pragma warning(disable:4996)
+
+using namespace std;
+
+
+#define endl "\n"
+#define int long long
+#define float double
+#define Debug(a,b) cout << a << " " << b << endl
+
+int Mul(int x, int y, int mod)
+{
+	return x * y % mod;
+}
+
+int GCD(int a, int b)
+{
+	if (a < b)
+		swap(a, b);
+
+	while (b != 0)
+	{
+		int r = a % b;
+		a = b;
+		b = r;
+	}
+	return a;
+}
+
+int LCM(int a, int b)
+{
+	return a * b / GCD(a, b);
+}
+
+int Ceil(double n)
+{
+	if (n - (int)(n) > 0)
+		return (int)n + 1;
+	else
+		return n;
+}
+
+int Pow(int a, int b)
+{
+	int res = 1;
+	while (b > 0)
+	{
+		if (b & 1)
+			res = (res * a);
+		b = b >> 1;
+		a = (a * a);
+	}
+	return res;
+}
+int Pow(int a, int b, int p)
+{
+	int res = 1;
+	a = a % p;
+	while (b > 0)
+	{
+		if (b & 1)
+			res = Mul(res, a, p);
+		b = b >> 1;
+		a = Mul(a, a, p);
+	}
+	return res;
+}
+
+const int INF = 0x3f3f3f3f;
+const int Dic[4][2] = { {+1,+0},{-1,+0},{0,+1},{0,-1} };
+
+////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+int N;
+int s = 1;
+int e = 1;
+int nowP = 1;
+void Print(int a, int b)
+{
+	for (int i = a; i <= b; i++)
+		Debug(nowP, i);
+	if (nowP == 1)
+		nowP = 2;
+	else
+		nowP = 1;
+}
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	std::cout.tie(NULL);
+
+	cin >> N;
+
+	cout << (N - 1) * (N) / 2 * 2 + 3 * N << endl;
+	bool flag0 = false;
+	int flag1 = 3;
+	while (s <= e || e != N)
+	{
+		if (e == N && flag1 > 0)
+		{
+			Print(s, e);
+			flag1--;
+			if (flag1 == 0)
+			{
+				s++;
+				flag0 = true;
+			}
+			continue;
+		}
+
+		if (!flag0)
+		{
+			Print(s, e);
+			e++;
+		}
+		else
+		{
+			Print(s, e);
+			s++;
+		}
+	}
+}
