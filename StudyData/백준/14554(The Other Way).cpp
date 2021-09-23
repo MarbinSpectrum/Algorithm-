21d72bc94989d6004be0e34ee9571cdb0d3f13f2
MarbinSpectrum <9507ym@naver.com>
2021-09-23 23:26:00 +0900

    14554

---
+++ StudyData/백준/14554(The Other Way).cpp
@@ -0,0 +1,217 @@
+#ifndef _GLIBCXX_NO_ASSERT 
+#include <cassert> 
+#endif #include <cctype>
+#include <cerrno> 
+#include <cfloat> 
+#include <ciso646> 
+#include <climits> 
+#include <clocale> 
+#include <cmath> 
+#include <csetjmp> 
+#include <csignal>
+#include <cstdarg> 
+#include <cstddef>
+#include <cstdio> 
+#include <cstdlib> 
+#include <cstring> 
+#include <ctime> 
+
+#if __cplusplus >= 201103L 
+#include <ccomplex> 
+#include <cfenv> 
+#include <cinttypes> 
+#include <cstdalign> 
+#include <cstdbool>
+#include <cstdint> 
+#include <ctgmath>
+#include <cwchar> 
+#include <cwctype>
+#endif 
+//
+//// C++ 
+#include <algorithm> 
+#include <bitset> 
+#include <complex> 
+#include <deque> 
+#include <exception> 
+#include <fstream> 
+#include <functional> 
+#include <iomanip> 
+#include <ios> 
+#include <iosfwd> 
+#include <iostream> 
+#include <istream> 
+#include <iterator> 
+#include <limits> 
+#include <list> 
+#include <locale> 
+#include <map> 
+#include <memory> 
+#include <new> 
+#include <numeric>
+#include <ostream> 
+#include <queue> 
+#include <set> 
+#include <sstream> 
+#include <stack> 
+#include <stdexcept> 
+#include <streambuf> 
+#include <string> 
+#include <typeinfo> 
+#include <utility> 
+#include <valarray> 
+#include <vector> 
+#include <unordered_map> 
+//
+#if __cplusplus >= 201103L 
+#include <array> 
+#include <atomic> 
+#include <chrono> 
+#include <condition_variable> 
+#include <forward_list> 
+#include <future> 
+#include <initializer_list> 
+#include <mutex> 
+#include <random> 
+#include <ratio>
+#include <regex> 
+#include <scoped_allocator> 
+#include <system_error> 
+#include <thread> 
+#include <tuple> 
+#include <typeindex> 
+#include <type_traits> 
+#include <unordered_map> 
+#include <unordered_set> 
+#endif
+
+#pragma warning(disable:4996)
+
+using namespace std;
+
+#define endl "\n"
+//#define int long long
+#define float double
+#define Debug(a,b) cout << a << " " << b << endl
+
+int Mul(int x, int y, int mod)
+{
+	return x * y % mod;
+}
+
+int GCD(int a, int b)
+{
+	if (a < b)
+		swap(a, b);
+
+	while (b != 0)
+	{
+		int r = a % b;
+		a = b;
+		b = r;
+	}
+	return a;
+}
+
+int LCM(int a, int b)
+{
+	return a * b / GCD(a, b);
+}
+
+int Ceil(double n)
+{
+	if (n - (int)(n) > 0)
+		return (int)n + 1;
+	else
+		return n;
+}
+
+int Pow(int a, int b)
+{
+	int res = 1;
+	while (b > 0)
+	{
+		if (b & 1)
+			res = (res * a);
+		b = b >> 1;
+		a = (a * a);
+	}
+	return res;
+}
+int Pow(int a, int b, int p)
+{
+	int res = 1;
+	a = a % p;
+	while (b > 0)
+	{
+		if (b & 1)
+			res = Mul(res, a, p);
+		b = b >> 1;
+		a = Mul(a, a, p);
+	}
+	return res;
+}
+
+const long long INF = 0x3f3f3f3f;
+const int Dic[4][2] = { {+1,+0},{-1,+0},{0,+1},{0,-1} };
+
+////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+int N, M, S, E;
+vector<pair<int, long long>> V[100001];
+priority_queue<pair<long long, int>> pq;
+long long Dist[100001];
+long long Ans[100001];
+const long long MOD = 1000000009;
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	std::cout.tie(NULL);
+
+	cin >> N >> M >> S >> E;
+	for (int i = 0; i < M; i++)
+	{
+		int a, b;
+		long long c;
+		cin >> a >> b >> c;
+		V[a].push_back({ b,c });
+		V[b].push_back({ a,c });
+	}
+
+	memset(Dist, INF, sizeof(Dist));
+
+	Dist[S] = 0;
+	Ans[S] = 1;
+	pq.push({ 0,S });
+	while (!pq.empty())
+	{
+		long long cost = -pq.top().first;
+		int now = pq.top().second;
+		pq.pop();
+
+		if (Dist[now] < cost)
+			continue;
+
+		for (int i = 0; i < V[now].size(); i++)
+		{
+			int next = V[now][i].first;
+			long long nextCost = cost + V[now][i].second;
+			if (Dist[next] < nextCost)
+				continue;
+			else if (Dist[next] == nextCost)
+				Ans[next] = (Ans[next] + Ans[now]) % MOD;
+			else
+			{
+				Dist[next] = nextCost;
+				Ans[next] = Ans[now];
+				pq.push({ -nextCost,next });
+			}
+		}
+	}
+
+	cout << Ans[E] << endl;
+}
