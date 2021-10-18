2c7e9a17eb52a7e4f99cffe1a5940bd4fa32c150
Youngmuk Kang <9507ym@naver.com>
2021-10-18 16:18:35 +0900

    Add files via upload

---
+++ StudyData/백준/23040(누텔라 트리 (Easy)).cpp
@@ -0,0 +1,234 @@
+﻿#ifndef _GLIBCXX_NO_ASSERT 
+#include <cassert> 
+#endif #include <cctype>
+#include <cerrno> 
+#include <cfloat> 
+#include <ciso646> 
+#include <climits> 
+#include <clocale> 
+#include <cmath> 
+#include <csetjmp> 
+#include <csignal>
+#include <cstdarg> 
+#include <cstddef>
+#include <cstdio> 
+#include <cstdlib> 
+#include <cstring> 
+#include <ctime> 
+
+#if __cplusplus >= 201103L 
+#include <ccomplex> 
+#include <cfenv> 
+#include <cinttypes> 
+#include <cstdalign> 
+#include <cstdbool>
+#include <cstdint> 
+#include <ctgmath>
+#include <cwchar> 
+#include <cwctype>
+#endif 
+//
+//// C++ 
+#include <algorithm> 
+#include <bitset> 
+#include <complex> 
+#include <deque> 
+#include <exception> 
+#include <fstream> 
+#include <functional> 
+#include <iomanip> 
+#include <ios> 
+#include <iosfwd> 
+#include <iostream> 
+#include <istream> 
+#include <iterator> 
+#include <limits> 
+#include <list> 
+#include <locale> 
+#include <map> 
+#include <memory> 
+#include <new> 
+#include <numeric>
+#include <ostream> 
+#include <queue> 
+#include <set> 
+#include <sstream> 
+#include <stack> 
+#include <stdexcept> 
+#include <streambuf> 
+#include <string> 
+#include <typeinfo> 
+#include <utility> 
+#include <valarray> 
+#include <vector> 
+#include <unordered_map> 
+//
+#if __cplusplus >= 201103L 
+#include <array> 
+#include <atomic> 
+#include <chrono> 
+#include <condition_variable> 
+#include <forward_list> 
+#include <future> 
+#include <initializer_list> 
+#include <mutex> 
+#include <random> 
+#include <ratio>
+#include <regex> 
+#include <scoped_allocator> 
+#include <system_error> 
+#include <thread> 
+#include <tuple> 
+#include <typeindex> 
+#include <type_traits> 
+#include <unordered_map> 
+#include <unordered_set> 
+#endif
+
+#pragma warning(disable:4996)
+
+using namespace std;
+
+#define endl "\n"
+#define int long long
+#define float double
+#define Debug(a,b) cout << a << " " << b << endl
+
+int Mul(int x, int y, int mod)
+{
+	return x * y % mod;
+}
+
+int GCD(int a, int b)
+{
+	if (a < b)
+		swap(a, b);
+
+	while (b != 0)
+	{
+		int r = a % b;
+		a = b;
+		b = r;
+	}
+	return a;
+}
+
+int LCM(int a, int b)
+{
+	return a * b / GCD(a, b);
+}
+
+int Ceil(double n)
+{
+	if (n - (int)(n) > 0)
+		return (int)n + 1;
+	else
+		return n;
+}
+
+int Pow(int a, int b)
+{
+	int res = 1;
+	while (b > 0)
+	{
+		if (b & 1)
+			res = (res * a);
+		b = b >> 1;
+		a = (a * a);
+	}
+	return res;
+}
+int Pow(int a, int b, int p)
+{
+	int res = 1;
+	a = a % p;
+	while (b > 0)
+	{
+		if (b & 1)
+			res = Mul(res, a, p);
+		b = b >> 1;
+		a = Mul(a, a, p);
+	}
+	return res;
+}
+
+const int INF = 0x3f3f3f3f;
+const int Dic[4][2] = { {+1,+0},{-1,+0},{0,+1},{0,-1} };
+
+////////////////////////////////////////////////////////////////////////
+
+int N;
+string C;
+vector<int> V[100001];
+int Value[100001];
+int Group[100001];
+bool Visit[100001];
+void DFS(int a, int& b, int& c)
+{
+	c++;
+	Visit[a] = true;
+	Group[a] = b;
+	for (int i = 0; i < V[a].size(); i++)
+	{
+		int next = V[a][i];
+		if (C[next] == 'R' && !Visit[next])
+		{
+			DFS(next, b, c);
+		}
+	}
+}
+int Get(int a)
+{
+	a = Group[a];
+	if (Value[a] != -1)
+	{
+		return Value[a];
+	}
+	Value[a] = 0;
+	DFS(a, a, Value[a]);
+	return Value[a];
+}
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	std::cout.tie(NULL);
+
+	cin >> N;
+	for (int i = 1; i <= N; i++)
+	{
+		Value[i] = -1;
+		Group[i] = i;
+	}
+	for (int i = 0; i < N - 1; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+
+		V[a].push_back(b);
+		V[b].push_back(a);
+	}
+	string temp;
+	C = " ";
+	cin >> temp;
+	C += temp;
+
+	int ans = 0;
+	for (int i = 1; i <= N; i++)
+	{
+		if (C[i] == 'B')
+		{
+			for (int j = 0; j < V[i].size(); j++)
+			{
+				int next = V[i][j];
+				if (C[next] == 'R')
+				{
+					ans += Get(next);
+				}
+			}
+		}
+	}
+
+	cout << ans << endl;
+}
