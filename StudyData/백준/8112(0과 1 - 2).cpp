8bdfd3b6d08d6460dceb34cb31c1fe6faca23789
MarbinSpectrum <9507ym@naver.com>
2021-12-29 21:08:09 +0900

    8112

---
+++ StudyData/백준/8112(0과 1 - 2).cpp
@@ -0,0 +1,266 @@
+#ifndef _GLIBCXX_NO_ASSERT 
+#include <cassert> 
+#endif #include <cctype>
+#include <cerrno> 
+#include <cfloat> 
+#include <ciso646> 
+#include <climits> 
+#include <clocale> 
+#include <cmath> 
+#include <csetjmp> 
+#include <csignal>
+#include <cstdarg> 
+#include <cstddef>
+#include <cstdio> 
+#include <cstdlib> 
+#include <cstring> 
+#include <ctime> 
+
+#if __cplusplus >= 201103L 
+#include <ccomplex> 
+#include <cfenv> 
+#include <cinttypes> 
+#include <cstdalign> 
+#include <cstdbool>
+#include <cstdint> 
+#include <ctgmath>
+#include <cwchar> 
+#include <cwctype>
+#endif 
+//
+//// C++ 
+#include <algorithm> 
+#include <bitset> 
+#include <complex> 
+#include <deque> 
+#include <exception> 
+#include <fstream> 
+#include <functional> 
+#include <iomanip> 
+#include <ios> 
+#include <iosfwd> 
+#include <iostream> 
+#include <istream> 
+#include <iterator> 
+#include <limits> 
+#include <list> 
+#include <locale> 
+#include <map> 
+#include <memory> 
+#include <new> 
+#include <numeric>
+#include <ostream> 
+#include <queue> 
+#include <set> 
+#include <sstream> 
+#include <stack> 
+#include <stdexcept> 
+#include <streambuf> 
+#include <string> 
+#include <typeinfo> 
+#include <utility> 
+#include <valarray> 
+#include <vector> 
+#include <unordered_map> 
+//
+#if __cplusplus >= 201103L 
+#include <array> 
+#include <atomic> 
+#include <chrono> 
+#include <condition_variable> 
+#include <forward_list> 
+#include <future> 
+#include <initializer_list> 
+#include <mutex> 
+#include <random> 
+#include <ratio>
+#include <regex> 
+#include <scoped_allocator> 
+#include <system_error> 
+#include <thread> 
+#include <tuple> 
+#include <typeindex> 
+#include <type_traits> 
+#include <unordered_map> 
+#include <unordered_set> 
+#endif
+
+#pragma warning(disable:4996)
+
+using namespace std;
+
+#define endl "\n"
+#define int long long
+#define float long double
+#define Debug(a,b) cout << a << " " << b << endl
+
+int Mul(int x, int y, int mod)
+{
+	return x * y % mod;
+}
+
+int GCD(int a, int b)
+{
+	if (a < b)
+		swap(a, b);
+
+	while (b != 0)
+	{
+		int r = a % b;
+		a = b;
+		b = r;
+	}
+	return a;
+}
+
+int LCM(int a, int b)
+{
+	return a * b / GCD(a, b);
+}
+int Ceil(int a, int b)
+{
+	return (a % b == 0) ? (a / b) : (a / b + 1);
+}
+int Ceil(double n)
+{
+	if (n - (int)(n) > 0)
+		return (int)n + 1;
+	else
+		return n;
+}
+
+int Pow(int a, int b)
+{
+	int res = 1;
+	while (b > 0)
+	{
+		if (b & 1)
+			res = (res * a);
+		b = b >> 1;
+		a = (a * a);
+	}
+	return res;
+}
+int Pow(int a, int b, int p)
+{
+	int res = 1;
+	a = a % p;
+	while (b > 0)
+	{
+		if (b & 1)
+			res = Mul(res, a, p);
+		b = b >> 1;
+		a = Mul(a, a, p);
+	}
+	return res;
+}
+
+vector<string> split(string input, vector<char> check)
+{
+	vector<string> answer;
+	input += check[0];
+	string temp = "";
+	for (int i = 0; i < input.size(); i++)
+	{
+		bool flag = false;
+		for (int j = 0; j < check.size(); j++)
+			if (input[i] == check[j])
+			{
+				flag = true;
+				break;
+			}
+		if (flag && temp.size() > 0)
+		{
+			answer.push_back(temp);
+			temp = "";
+		}
+		else
+		{
+			temp += input[i];
+		}
+	}
+	return answer;
+}
+
+const int INF = 0x3f3f3f3f;
+const int Dic[4][2] = { {+1,+0},{-1,+0},{0,+1},{0,-1} };
+
+////////////////////////////////////////////////////////////////////////
+
+int T;
+//방문여부,부모노드,현재상태
+tuple<bool, int, int> Visit[1000001];
+
+int F(int N)
+{
+	queue<int> Q;
+	Q.push(1);
+	Visit[1] = { true,-1,1 };
+	while (!Q.empty())
+	{
+		int temp = Q.front();
+		Q.pop();
+
+		if (temp == 0)
+			return temp;
+
+		for (int i = 0; i < 2; i++)
+		{
+			int v = (temp * 10 + i) % N;
+
+			bool a = get<0>(Visit[v]);
+			int b = get<1>(Visit[v]);
+			int c = get<2>(Visit[v]);
+
+			if (!a)
+			{
+				a = true;
+				b = temp;
+				c = i;
+				Visit[v] = { a,b,c };
+				Q.push(v);
+			}
+		}
+	}
+
+	return -1;
+}
+void PrintAns(int n)
+{
+	if (n == -1)
+		return;
+	PrintAns(get<1>(Visit[n]));
+	cout << get<2>(Visit[n]);
+}
+int32_t main()
+{
+	ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+
+	cin >> T;
+	while (T--)
+	{
+		int N;
+		cin >> N;
+		if (N == 1)
+		{
+			cout << 1 << endl;
+		}
+		else
+		{
+			for (int i = 0; i < 1000001; i++)
+			{
+				Visit[i] = { false,-1,-1 };
+			}
+			int ans = F(N);
+			if (ans == -1)
+			{
+				cout << "BBAK" << endl;
+				continue;
+			}
+			PrintAns(ans);
+			cout << endl;
+		}
+	}
+}
