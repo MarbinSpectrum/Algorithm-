7873cf5c8e1b1e11acf731dd77cf4b21f73818ad
MarbinSpectrum <9507ym@naver.com>
2022-10-04 23:41:27 +0900

    15775

---
+++ StudyData/백준/15775(Voronoi Diagram).cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#pragma warning(disable:4996)
+
+using namespace std;
+
+#define endl "\n"
+#define int long long
+#define float long double
+
+const int INF = 999999999999999;
+const int Dic[4][2] = { {0,1},{0,-1},{1,0},{-1,0} };
+//////////////////////////////////////////////////////////////////////// 
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+}
